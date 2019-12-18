86beab3fe1e2fcefea25d52143de7c70d4bc2b7d
Rijnard van Tonder <rvantonder@gmail.com>
2019-12-17 21:08:29 -0700

    proof of concept context lines

--- cmd/src/search.go
+++ cmd/src/search.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"html"
 	"io"
+	"math"
 	"os"
 	"os/exec"
 	"regexp"
@@ -107,6 +108,7 @@ Other tips:
 					name
 					path
 					url
+					content
 					commit {
 						oid
 					}
@@ -419,6 +421,42 @@ type highlight struct {
 	length    int // the 1-indexed length of the highlight, in characters.
 }
 
+// applyHighlightsForFile expects highlight information that is
+// relative to the whole file, and can add lines of context around the
+// highlighted range. It makes no assumptions about the preview field in
+// LineMatches (the preview field is not used). It uses contextLines to show
+// surrounding lines of context for matches.
+func applyHighlightsForFile(fileContent *string, highlights []highlight, contextLines int) string {
+	var result []rune
+	start := ansiColors["search-match"]
+	end := ansiColors["nc"]
+	lines := strings.Split(*fileContent, "\n")
+	minLine := math.MaxInt32
+	maxLine := 0
+	for _, highlight := range highlights {
+		if highlight.line > maxLine {
+			maxLine = highlight.line
+		}
+		if highlight.line < minLine {
+			minLine = highlight.line
+		}
+		line := lines[highlight.line]
+		for characterIndex, character := range []rune(line + "\n") {
+			if characterIndex == highlight.character {
+				result = append(result, []rune(start)...)
+			} else if characterIndex == highlight.character+highlight.length {
+				result = append(result, []rune(end)...)
+			}
+			result = append(result, character)
+		}
+	}
+	contextBefore := strings.Join(lines[minLine-contextLines:minLine], "\n")
+	contextAfter := strings.Join(lines[maxLine+1:maxLine+contextLines+1], "\n")
+	return (contextBefore + "\n" + string(result) + "      " + contextAfter + "\n")
+}
+
+// applyHighlights expects highlight information that applies relative to lines in
+// the input string, where the input string corresponds to the preview field in LineMatches.
 func applyHighlights(input string, highlights []highlight, start, end string) string {
 	var result []rune
 	lines := strings.Split(input, "\n")
@@ -440,6 +478,27 @@ func applyHighlights(input string, highlights []highlight, start, end string) st
 	return string(result)
 }
 
+// convertMatchToHighlights converts a FileMatch m to a highlight data type.
+// When isPreview is true, it is assumed that the result to highlight is only on
+// one line, and the offets are relative to this line. When isPreview is false,
+// the lineNumber from the FileMatch data is used, which is relative to the file
+// content.
+func convertMatchToHighlights(m map[string]interface{}, isPreview bool) (highlights []highlight) {
+	var line int
+	for _, offsetAndLength := range m["offsetAndLengths"].([]interface{}) {
+		ol := offsetAndLength.([]interface{})
+		offset := int(ol[0].(float64))
+		length := int(ol[1].(float64))
+		if isPreview {
+			line = 1
+		} else {
+			line = int(m["lineNumber"].(float64))
+		}
+		highlights = append(highlights, highlight{line: line, character: offset, length: length})
+	}
+	return highlights
+}
+
 var searchTemplateFuncs = map[string]interface{}{
 	"searchSequentialLineNumber": func(lineMatches []interface{}, index int) bool {
 		prevIndex := index - 1
@@ -450,17 +509,20 @@ var searchTemplateFuncs = map[string]interface{}{
 		lineNumber := lineMatches[index].(map[string]interface{})["lineNumber"]
 		return prevLineNumber.(float64) == lineNumber.(float64)-1
 	},
-	"searchHighlightMatch": func(match interface{}) string {
+	"searchHighlightMatch": func(content, query, match interface{}) string {
 		m := match.(map[string]interface{})
-		preview := m["preview"].(string)
+		q := query.(string)
 		var highlights []highlight
-		for _, offsetAndLength := range m["offsetAndLengths"].([]interface{}) {
-			ol := offsetAndLength.([]interface{})
-			offset := int(ol[0].(float64))
-			length := int(ol[1].(float64))
-			highlights = append(highlights, highlight{line: 1, character: offset, length: length})
+		if strings.Contains(q, "patterntype:structural") {
+			highlights = convertMatchToHighlights(m, false)
+			c := content.(string)
+			context := 3
+			return applyHighlightsForFile(&c, highlights, context)
+		} else {
+			preview := m["preview"].(string)
+			highlights = convertMatchToHighlights(m, true)
+			return applyHighlights(preview, highlights, ansiColors["search-match"], ansiColors["nc"])
 		}
-		return applyHighlights(preview, highlights, ansiColors["search-match"], ansiColors["nc"])
 	},
 	"searchHighlightPreview": func(preview interface{}) string {
 		return searchHighlightPreview(preview, "", "")
@@ -549,12 +611,13 @@ const searchResultsTemplate = `{{- /* ignore this line for template formatting s
 
 			{{- /* Line matches */ -}}
 			{{- $lineMatches := .lineMatches -}}
+			{{- $content := .file.content -}}
 			{{- range $index, $match := $lineMatches -}}
 				{{- if not (searchSequentialLineNumber $lineMatches $index) -}}
 					{{- color "search-border"}}{{"  ------------------------------------------------------------------------------\n"}}{{color "nc"}}
 				{{- end -}}
 				{{- "  "}}{{color "search-line-numbers"}}{{pad (addFloat $match.lineNumber 1) 6 " "}}{{color "nc" -}}
-				{{- color "search-border"}}{{" |  "}}{{color "nc"}}{{searchHighlightMatch $match}}
+				{{- color "search-border"}}{{" |  "}}{{color "nc"}}{{searchHighlightMatch $content $.Query $match}}
 			{{- end -}}
 		{{- end -}}
 
