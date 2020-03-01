2bc12c4fa52fe114dff7671653db18df0016cce3
Erik Seliger <erikseliger@me.com>
2020-03-01 04:49:26 +0100

    Fix

--- cmd/src/actions_runner.go
+++ cmd/src/actions_runner.go
@@ -270,7 +270,7 @@ func (r *runner) runActionJob(_ctx context.Context, job *actionJob) (*string, er
 	}
 
 	logBuffer.Write([]byte("Computing diff.."))
-	d, err := computeDiff(runCtx, x.zipFile, x.volumeDir, "test")
+	d, err := x.computeDiff()
 	if err != nil {
 		logBuffer.Write([]byte(fmt.Sprintf("Failed to compute diff: %s\n", err.Error())))
 		return nil, errors.Wrap(err, "failed to compute diff")
