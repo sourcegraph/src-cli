0ae6017747647119351f1d2f65f95bba80771bad
Uwe Hoffmann <534011+uwedeportivo@users.noreply.github.com>
2020-05-08 09:45:44 -0700

    fleshed out a bit more

--- cmd/src/validate.go
+++ cmd/src/validate.go
@@ -5,8 +5,11 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"os"
+	"strings"
 	"time"
 
+	"github.com/mattn/go-isatty"
 	"github.com/starlight-go/starlight"
 )
 
@@ -19,13 +22,9 @@ EXPERIMENTAL: 'validate' is an experimental command in the 'src' tool.
 
 Usage:
 
-	src validate <script-file.star>
-
-Examples:
-
-  Validate the Sourcegraph instance with the starlark script: 'validate.star'
-
-    	$ src validate validate.star
+	src validate [options] <script-file>
+or
+    cat <script-file> | src validate [options]
 `
 	flagSet := flag.NewFlagSet("validate", flag.ExitOnError)
 	usageFunc := func() {
@@ -34,35 +33,77 @@ Examples:
 		fmt.Println(usage)
 	}
 
+	var (
+		contextFlag = flagSet.String("context", "", `Comma-separated list of key=value pairs to add to the script execution context`)
+		docFlag = flagSet.Bool("doc", false, `Show function documentation`)
+	)
+
 	vd := &validator{}
 
 	commands = append(commands, &command{
 		flagSet: flagSet,
 		handler: func(args []string) error {
-			if len(args) != 1 {
-				return errors.New("no script argument")
+			if *docFlag {
+				vd.printDocumentation()
+				return nil
 			}
-			return vd.validate(args[0])
+
+			var script []byte
+			var err error
+			if len(flagSet.Args()) == 1 {
+				script, err = ioutil.ReadFile(flagSet.Arg(0))
+				if err != nil {
+					return err
+				}
+			}
+			if !isatty.IsTerminal(os.Stdin.Fd()) {
+				// stdin is a pipe not a terminal
+				script, err = ioutil.ReadAll(os.Stdin)
+				if err != nil {
+					return err
+				}
+			}
+
+			return vd.validate(script, vd.parseScriptContext(*contextFlag))
 		},
 		usageFunc: usageFunc,
 	})
 }
 
-func (vd *validator) validate(path string) error {
-	script, err := ioutil.ReadFile(path)
-	if err != nil {
-		return err
-	}
+func (vd *validator) printDocumentation() {
+	fmt.Println("TODO(uwedeportivo): write function documentation")
+}
 
+func (vd *validator) parseScriptContext(val string) map[string]string {
+	scriptContext := make(map[string]string)
+
+	pairs := strings.Split(val, ",")
+	for _, pair := range pairs {
+		kv := strings.Split(pair, "=")
+
+		if len(kv) == 2 {
+			scriptContext[kv[0]] = kv[1]
+		}
+	}
+	return scriptContext
+}
+
+func (vd *validator) validate(script []byte, scriptContext map[string]string) error {
 	globals := map[string]interface{}{
-		"sg_add_external_service": vd.addExternalService,
-		"sg_search": vd.searchMatchCount,
-		"sg_repo_cloned": vd.repoCloned,
-		"sg_sleep_seconds": vd.sleepSeconds,
+		"src_add_external_service": vd.addExternalService,
+		"src_delete_external_service": vd.deleteExternalService,
+		"src_search_match_count": vd.searchMatchCount,
+		"src_list_repos": vd.listRepos,
+		"src_sleep_seconds": vd.sleepSeconds,
+		"src_log": vd.log,
+		"src_run_graphql": vd.runGraphQL,
+		"src_context": scriptContext,
 	}
 
 	vals, err := starlight.Eval(script, globals, nil)
-
+	if err != nil {
+		return err
+	}
 	if passed, ok := vals["passed"].(bool); !ok || !passed {
 		return errors.New("failed")
 	}
@@ -83,8 +124,8 @@ mutation AddExternalService($kind: ExternalServiceKind!, $displayName: String!, 
   {
     id
   }
-}
-`
+}`
+
 func (vd *validator) addExternalService(kind, displayName, config string) (string, error) {
 	var resp struct {
 		Data struct {
@@ -106,7 +147,28 @@ func (vd *validator) addExternalService(kind, displayName, config string) (strin
 	return resp.Data.AddExternalService.ID, err
 }
 
-const vdSearchQuery = `
+const vdDeleteExternalServiceQuery = `
+mutation DeleteExternalService($id: ID!) {
+  deleteExternalService(externalService: $id){
+    alwaysNil
+  } 
+}`
+
+func (vd *validator) deleteExternalService(id string) error {
+	var resp struct {}
+
+	err := (&apiRequest{
+		query: vdDeleteExternalServiceQuery,
+		vars: map[string]interface{}{
+			"id": id,
+		},
+		result: &resp,
+	}).do()
+
+	return err
+}
+
+const vdSearchMatchCountQuery = `
 query ($query: String!) {
   search(query: $query, version: V2, patternType:literal) {
     results {
@@ -125,7 +187,7 @@ func (vd *validator) searchMatchCount(searchStr string) (int, error) {
 	}
 
 	err := (&apiRequest{
-		query: vdSearchQuery,
+		query: vdSearchMatchCountQuery,
 		vars: map[string]interface{}{
 			"query": searchStr,
 		},
@@ -135,13 +197,12 @@ func (vd *validator) searchMatchCount(searchStr string) (int, error) {
 	return resp.Search.Results.MatchCount, err
 }
 
-const vdRepoCloning = `
-query RepoCloning($repoName:String!) {
+const vdListRepos = `
+query ListRepos($cloneInProgress: Boolean!, $cloned: Boolean!, $notCloned: Boolean!) {
   repositories(
-    cloneInProgress: false,
-    cloned: true,
-    notCloned: false,
-    names:[$repoName],
+    cloned: $cloneInProgress
+    cloneInProgress: $cloned
+    notCloned: $notCloned
   ) {
     nodes {
       name
@@ -149,7 +210,7 @@ query RepoCloning($repoName:String!) {
   }
 }`
 
-func (vd *validator) repoCloned(repoName string) (bool, error) {
+func (vd *validator) listRepos(cloneInProgress, cloned, notCloned bool) ([]string, error) {
 	var resp struct {
 		Repositories struct {
 			Nodes []struct {
@@ -159,12 +220,36 @@ func (vd *validator) repoCloned(repoName string) (bool, error) {
 	}
 
 	err := (&apiRequest{
-		query: vdRepoCloning,
+		query: vdListRepos,
 		vars: map[string]interface{}{
-			"repoName": repoName,
+			"cloneInProgress": cloneInProgress,
+			"cloned": cloned,
+			"notCloned": notCloned,
 		},
 		result: &resp,
 	}).do()
 
-	return len(resp.Repositories.Nodes) == 1, err
+	names := make([]string, 0, len(resp.Repositories.Nodes))
+	for _, node := range resp.Repositories.Nodes {
+		names = append(names, node.Name)
+	}
+
+	return names, err
 }
+
+func (vd *validator) log(line string) {
+	fmt.Println(line)
+}
+
+func (vd *validator) runGraphQL(query string, vars map[string]interface{}) (map[string]interface{}, error) {
+	resp := map[string]interface{}{}
+
+	err := (&apiRequest{
+		query: query,
+		vars: vars,
+		result: &resp,
+	}).do()
+
+	return resp, err
+}
+
