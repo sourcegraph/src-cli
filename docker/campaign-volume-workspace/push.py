e74781f4dfec0d87a0bc7fde8ba442b2884243d1
Adam Harvey <adam@adamharvey.name>
2020-12-31 13:45:38 -0800

    Replace the canned Action with a script.

---
+++ docker/campaign-volume-workspace/push.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+
+import argparse
+import itertools
+import os
+import subprocess
+
+from typing import BinaryIO, Sequence
+
+
+def calculate_tags(ref: str) -> Sequence[str]:
+    # The tags always include latest.
+    tags = ["latest"]
+
+    # If the ref is a tag ref, then we should parse the version out.
+    if ref.startswith("refs/tags/"):
+        tags.extend(
+            [
+                ".".join(vc)
+                for vc in itertools.accumulate(
+                    ref.split("/", 2)[2].split("."),
+                    lambda vlist, v: vlist + [v],
+                    initial=[],
+                )
+                if len(vc) > 0
+            ]
+        )
+
+    return tags
+
+
+def docker_build(dockerfile: BinaryIO, image: str, tags: Sequence[str]):
+    run(
+        [
+            "docker",
+            "build",
+            *itertools.chain(*[("-t", f"{image}:{tag}") for tag in tags]),
+            "-",
+        ],
+        stdin=dockerfile,
+    )
+
+
+def docker_login(username: str, password: str):
+    run(
+        ["docker", "login", f"-u={username}", "--password-stdin"],
+        input=password,
+    )
+
+
+def docker_push(image: str, tags: Sequence[str]):
+    for tag in tags:
+        subprocess.run(["docker", "push", f"{image}:{tag}"])
+
+
+def run(args: Sequence[str], /, **kwargs) -> subprocess.CompletedProcess:
+    print(f"+ {' '.join(args)}")
+    return subprocess.run(args, **kwargs)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-d", "--dockerfile", required=True, help="the Dockerfile to build"
+    )
+    parser.add_argument("-i", "--image", required=True, help="Docker image to push")
+    parser.add_argument(
+        "-r",
+        "--ref",
+        default=os.environ.get("GITHUB_REF"),
+        help="current ref in refs/heads/... or refs/tags/... form",
+    )
+    args = parser.parse_args()
+
+    tags = calculate_tags(args.ref)
+    print(f"will push tags: {', '.join(tags)}")
+
+    print("building image")
+    docker_build(open(args.dockerfile, "rb"), args.image, tags)
+
+    print("logging into Docker Hub")
+    try:
+        docker_login(os.environ["DOCKER_USERNAME"], os.environ["DOCKER_PASSWORD"])
+    except KeyError as e:
+        print(f"error retrieving environment variables: {e}")
+        raise
+
+    print("pushing images to Docker Hub")
+    docker_push(args.image, tags)
+
+    print("success!")
+
+
+if __name__ == "__main__":
+    main()
