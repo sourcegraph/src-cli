203868dd9ce3ecf442638b1bfee906f3916c7472
Adam Harvey <adam@adamharvey.name>
2021-01-07 13:38:03 -0800

    Add documentation.

--- internal/campaigns/repo_fetcher.go
+++ internal/campaigns/repo_fetcher.go
@@ -10,10 +10,17 @@ import (
 	"github.com/sourcegraph/src-cli/internal/campaigns/graphql"
 )
 
+// RepoFetcher abstracts the process of retrieving an archive for the given
+// repository.
 type RepoFetcher interface {
+	// Fetch must retrieve the given repository and return it as a RepoZip.
+	// This will generally imply that the file should be written to a temporary
+	// location on the filesystem.
 	Fetch(context.Context, *graphql.Repository) (RepoZip, error)
 }
 
+// repoFetcher is the concrete implementation of the RepoFetcher interface used
+// outside of tests.
 type repoFetcher struct {
 	client     api.Client
 	dir        string
@@ -22,11 +29,18 @@ type repoFetcher struct {
 
 var _ RepoFetcher = &repoFetcher{}
 
+// RepoZip implementations represent a downloaded repository archive.
 type RepoZip interface {
+	// Close must finalise the downloaded archive. If one or more temporary
+	// files were created, they should be deleted here.
 	Close() error
+
+	// Path must return the path to the archive on the filesystem.
 	Path() string
 }
 
+// repoZip is the concrete implementation of the RepoZip interface used outside
+// of tests.
 type repoZip struct {
 	path    string
 	fetcher *repoFetcher
