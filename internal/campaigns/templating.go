9eb66408bb52a0581777e9212e28374f99f8c38a
Thorsten Ball <mrnugget@gmail.com>
2021-01-11 15:22:31 +0100

    Introduce tiny helper

--- internal/campaigns/templating.go
+++ internal/campaigns/templating.go
@@ -3,14 +3,25 @@ package campaigns
 import (
 	"bytes"
 	"fmt"
+	"io"
 	"strings"
 	"text/template"
 
+	"github.com/pkg/errors"
 	"github.com/sourcegraph/src-cli/internal/campaigns/graphql"
 )
 
 func parseAsTemplate(name, input string, stepCtx *StepContext) (*template.Template, error) {
 	return template.New(name).Delims("${{", "}}").Funcs(stepCtx.ToFuncMap()).Parse(input)
+}
+
+func renderTemplate(name, tmpl string, out io.Writer, stepCtx *StepContext) error {
+	t, err := parseAsTemplate(name, tmpl, stepCtx)
+	if err != nil {
+		return errors.Wrap(err, "parsing step run")
+	}
+
+	return t.Execute(out, stepCtx)
 }
 
 func renderMap(m map[string]string, stepCtx *StepContext) (map[string]string, error) {
