sourcegraph/sourcegraph-public-snapshot

commit 13f99c5a885dab3ebba745314d3bbdc464024a62

Handle interrupt signal in src campaigns apply|preview (#328)
Before this change Docker containers kept on running after the interrupt
signal was sent to the process and killing it. This was a slight
regression, since it was previously fixed by #169.

What this commit does is bring back the behaviour of #169 and also makes
sure that we print the correct error message when we canceled a running
step.

It also makes sure that we don't enqueue more tasks/steps after being
canceled: that's the code in `(*executor).Start()` that aborts the
`Range` call if the context has been cancelled/timed-out and aborts
execution of a task if the goroutine already blocked on
`x.par.Acquire()`.

internal/campaigns/executor.go
@@ -19,6 +19,10 @@ type TaskExecutionErr struct {
 	Repository string
 }
 
+func (e TaskExecutionErr) Cause() error {
+	return e.Err
+}
+
 func (e TaskExecutionErr) Error() string {
 	return fmt.Sprintf(
 		"execution in %s failed: %s (see %s for details)",
@@ -110,13 +114,25 @@ func (x *executor) LogFiles() []string {
 
 func (x *executor) Start(ctx context.Context) {
 	x.tasks.Range(func(k, v interface{}) bool {
+		select {
+		case <-ctx.Done():
+			return false
+		default:
+		}
+
 		x.par.Acquire()
 
 		go func(task *Task) {
 			defer x.par.Release()
-			err := x.do(ctx, task)
-			if err != nil {
-				x.par.Error(err)
+
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				err := x.do(ctx, task)
+				if err != nil {
+					x.par.Error(err)
+				}
 			}
 		}(k.(*Task))
 

cmd/src/campaigns_apply.go
@@ -56,7 +56,10 @@ Examples:
 		}
 
 		out := output.NewOutput(flagSet.Output(), output.OutputOpts{Verbose: *verbose})
-		ctx := context.Background()
+
+		ctx, cancel := contextCancelOnInterrupt(context.Background())
+		defer cancel()
+
 		svc := campaigns.NewService(&campaigns.ServiceOpts{
 			AllowUnsupported: flags.allowUnsupported,
 			Client:           cfg.apiClient(flags.api, flagSet.Output()),

cmd/src/campaigns_common.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"os"
 	"os/exec"
+	"os/signal"
 	"path"
 	"runtime"
 	"strings"
@@ -24,6 +25,7 @@ import (
 var (
 	campaignsPendingColor = output.StylePending
 	campaignsSuccessColor = output.StyleSuccess
+	campaignsErrorColor   = output.StyleWarning
 	campaignsSuccessEmoji = output.EmojiSuccess
 )
 
@@ -272,7 +274,9 @@ func campaignsExecute(ctx context.Context, out *output.Output, svc *campaigns.Se
 		for _, ts := range unloggedCompleted {
 			var statusText string
 
-			if ts.ChangesetSpec == nil {
+			if ts.Err != nil {
+				statusText = fmt.Sprintf("%s%s%s", campaignsErrorColor, "Errored", output.StyleReset)
+			} else if ts.ChangesetSpec == nil {
 				statusText = "No changes"
 			} else {
 				fileDiffs, err := diff.ParseMultiFileDiff([]byte(ts.ChangesetSpec.Commits[0].Diff))
@@ -375,6 +379,15 @@ func printExecutionError(out *output.Output, err error) {
 }
 
 func formatTaskExecutionErr(err campaigns.TaskExecutionErr) string {
+	if ee, ok := errors.Cause(err).(*exec.ExitError); ok && ee.String() == "signal: killed" {
+		return fmt.Sprintf(
+			"%s%s%s: killed by interrupt signal",
+			output.StyleBold,
+			err.Repository,
+			output.StyleReset,
+		)
+	}
+
 	return fmt.Sprintf(
 		"%s%s%s:\n%s\nLog: %s\n",
 		output.StyleBold,
@@ -431,4 +444,23 @@ func checkExecutable(cmd string, args ...string) error {
 		)
 	}
 	return nil
+}
+
+func contextCancelOnInterrupt(parent context.Context) (context.Context, func()) {
+	ctx, ctxCancel := context.WithCancel(parent)
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+
+	go func() {
+		select {
+		case <-c:
+			ctxCancel()
+		case <-ctx.Done():
+		}
+	}()
+
+	return ctx, func() {
+		signal.Stop(c)
+		ctxCancel()
+	}
 }

cmd/src/campaigns_preview.go
@@ -33,7 +33,10 @@ Examples:
 		}
 
 		out := output.NewOutput(flagSet.Output(), output.OutputOpts{Verbose: *verbose})
-		ctx := context.Background()
+
+		ctx, cancel := contextCancelOnInterrupt(context.Background())
+		defer cancel()
+
 		svc := campaigns.NewService(&campaigns.ServiceOpts{
 			AllowUnsupported: flags.allowUnsupported,
 			Client:           cfg.apiClient(flags.api, flagSet.Output()),

internal/campaigns/run_steps.go
@@ -215,6 +215,8 @@ type stepFailedErr struct {
 	Err error
 }
 
+func (e stepFailedErr) Cause() error { return e.Err }
+
 func (e stepFailedErr) Error() string {
 	var out strings.Builder
 

sourcegraph/src-cli

commit 3f6f965410573c61944d9c4905358470463ee05d

Correctly abort on interrupt instead of going into endless loop
Running `:GoMetaLinter` just pointed me to this "ineffective break
statement" that I introduced in #328 with the following comment:

> It also makes sure that we don't enqueue more tasks/steps after being
> canceled: that's the code in `(*executor).Start()` that aborts the
> `Range` call if the context has been cancelled/timed-out and aborts
> execution of a task if the goroutine already blocked on
> `x.par.Acquire()`.

Well... turns out that wasn't the case, was it?

Instead the code only broke out of the `select` and continued trying to
acquire a lock.

I also suspect that this bug might have something to do with a customer
reporting that long-running containers are not stopped when hitting
Ctrl-C (https://github.com/sourcegraph/sourcegraph/issues/16026) even
after we thought the issue was fixed with #328. I'm not 100% sure on
this one though.

internal/campaigns/executor.go
@@ -192,10 +192,12 @@ func (x *executor) LogFiles() []string {
 }
 
 func (x *executor) Start(ctx context.Context) {
+	defer func() { close(x.doneEnqueuing) }()
+
 	for _, task := range x.tasks {
 		select {
 		case <-ctx.Done():
-			break
+			return
 		default:
 		}
 
@@ -215,8 +217,6 @@ func (x *executor) Start(ctx context.Context) {
 			}
 		}(task)
 	}
-
-	close(x.doneEnqueuing)
 }
 
 func (x *executor) Wait() ([]*ChangesetSpec, error) {