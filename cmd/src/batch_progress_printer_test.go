sourcegraph/sourcegraph-public-snapshot

commit d6c876cd32397ad53f1e5df1a850fcac7076ba3e

Introduce executor.Coordinator and split up responsibilities between Coordinator/Service/executor (#536)
* Broken build with TaskStatusHubThing

* Introduce Coordinator

* Replace TaskStatusHubThing with TaskStatusCollection

* Clean up the executor

* Clean up handling of ExecutionOpts

* Fix missing rename

* Make executor property and status a runtime arg

* Create separate tests for Coordinator

* Fix tests for Coordinator

* More test cleanup

* Re-add missing test

* Re-add transfromGroup test but its passing, what

* Add test for transform group

* Add test helpers

* Backport fix from #539

* Update internal/batches/executor/coordinator.go

Co-authored-by: Adam Harvey <aharvey@sourcegraph.com>

Co-authored-by: Adam Harvey <aharvey@sourcegraph.com>

cmd/src/batch_progress_printer.go
@@ -274,10 +274,6 @@ func taskStatusBarText(ts *executor.TaskStatus) (string, error) {
 				statusText = "No changes"
 			}
 		}
-
-		if ts.Cached {
-			statusText += " (cached)"
-		}
 	} else if ts.IsRunning() {
 		if ts.CurrentlyExecuting != "" {
 			lines := strings.Split(ts.CurrentlyExecuting, "\n")

internal/batches/executor/coordinator.go
@@ -1,0 +1,269 @@
+package executor
+
+import (
+	"context"
+	"reflect"
+	"strconv"
+	"time"
+
+	"github.com/hashicorp/go-multierror"
+	"github.com/pkg/errors"
+	"github.com/sourcegraph/src-cli/internal/api"
+	"github.com/sourcegraph/src-cli/internal/batches"
+	"github.com/sourcegraph/src-cli/internal/batches/graphql"
+	"github.com/sourcegraph/src-cli/internal/batches/log"
+	"github.com/sourcegraph/src-cli/internal/batches/workspace"
+)
+
+type taskExecutor interface {
+	Start(context.Context, []*Task, taskStatusHandler)
+	Wait(context.Context) ([]taskResult, error)
+}
+
+// Coordinates coordinates the execution of Tasks. It makes use of an executor,
+// checks the ExecutionCache whether execution is necessary, builds
+// batches.ChangesetSpecs out of the executionResults.
+type Coordinator struct {
+	opts NewCoordinatorOpts
+
+	cache      ExecutionCache
+	exec       taskExecutor
+	logManager *log.Manager
+}
+
+type repoNameResolver func(ctx context.Context, name string) (*graphql.Repository, error)
+
+type NewCoordinatorOpts struct {
+	// Dependencies
+	ResolveRepoName repoNameResolver
+	Creator         workspace.Creator
+	Client          api.Client
+
+	// Everything that follows are either command-line flags or features.
+
+	// TODO: We could probably have a wrapper around flags and features,
+	// something like ExecutionArgs, that we can pass around
+	CacheDir   string
+	ClearCache bool
+	SkipErrors bool
+
+	// Used by createChangesetSpecs
+	AutoAuthorDetails bool
+
+	CleanArchives bool
+	Parallelism   int
+	Timeout       time.Duration
+	KeepLogs      bool
+	TempDir       string
+}
+
+func NewCoordinator(opts NewCoordinatorOpts) *Coordinator {
+	cache := NewCache(opts.CacheDir)
+	logManager := log.NewManager(opts.TempDir, opts.KeepLogs)
+
+	exec := newExecutor(newExecutorOpts{
+		Fetcher: batches.NewRepoFetcher(opts.Client, opts.CacheDir, opts.CleanArchives),
+		Creator: opts.Creator,
+		Logger:  logManager,
+
+		AutoAuthorDetails: opts.AutoAuthorDetails,
+		Parallelism:       opts.Parallelism,
+		Timeout:           opts.Timeout,
+		TempDir:           opts.TempDir,
+	})
+
+	return &Coordinator{
+		opts: opts,
+
+		cache:      cache,
+		exec:       exec,
+		logManager: logManager,
+	}
+}
+
+// CheckCache checks whether the internal ExecutionCache contains
+// ChangesetSpecs for the given Tasks. If cached ChangesetSpecs exist, those
+// are returned, otherwise the Task, to be executed later.
+func (c *Coordinator) CheckCache(ctx context.Context, tasks []*Task) (uncached []*Task, specs []*batches.ChangesetSpec, err error) {
+	for _, t := range tasks {
+		cachedSpecs, found, err := c.checkCacheForTask(ctx, t)
+		if err != nil {
+			return nil, nil, err
+		}
+
+		if !found {
+			uncached = append(uncached, t)
+			continue
+		}
+
+		specs = append(specs, cachedSpecs...)
+	}
+
+	return uncached, specs, nil
+}
+
+func (c *Coordinator) checkCacheForTask(ctx context.Context, task *Task) (specs []*batches.ChangesetSpec, found bool, err error) {
+	// Check if the task is cached.
+	cacheKey := task.cacheKey()
+	if c.opts.ClearCache {
+		if err := c.cache.Clear(ctx, cacheKey); err != nil {
+			return specs, false, errors.Wrapf(err, "clearing cache for %q", task.Repository.Name)
+		}
+
+		return specs, false, nil
+	}
+
+	var result executionResult
+	result, found, err = c.cache.Get(ctx, cacheKey)
+	if err != nil {
+		return specs, false, errors.Wrapf(err, "checking cache for %q", task.Repository.Name)
+	}
+
+	if !found {
+		return specs, false, nil
+	}
+
+	// If the cached result resulted in an empty diff, we don't need to
+	// add it to the list of specs that are displayed to the user and
+	// send to the server. Instead, we can just report that the task is
+	// complete and move on.
+	if result.Diff == "" {
+		return specs, true, nil
+	}
+
+	specs, err = createChangesetSpecs(task, result, c.opts.AutoAuthorDetails)
+	if err != nil {
+		return specs, false, err
+	}
+
+	return specs, true, nil
+}
+
+func (c *Coordinator) cacheAndBuildSpec(ctx context.Context, taskResult taskResult, status taskStatusHandler) ([]*batches.ChangesetSpec, error) {
+	// Add to the cache, even if no diff was produced.
+	cacheKey := taskResult.task.cacheKey()
+	if err := c.cache.Set(ctx, cacheKey, taskResult.result); err != nil {
+		return nil, errors.Wrapf(err, "caching result for %q", taskResult.task.Repository.Name)
+	}
+
+	// If the steps didn't result in any diff, we don't need to create a
+	// changeset spec that's displayed to the user and send to the server.
+	if taskResult.result.Diff == "" {
+		return nil, nil
+	}
+
+	// Build the changeset specs.
+	specs, err := createChangesetSpecs(taskResult.task, taskResult.result, c.opts.AutoAuthorDetails)
+	if err != nil {
+		return specs, err
+	}
+
+	// Update the status of Task
+	status.Update(taskResult.task, func(status *TaskStatus) {
+		status.ChangesetSpecs = specs
+		// TODO(mrnugget): Ideally we'd get rid of this dependency on the task
+		// status handler here.
+	})
+
+	return specs, nil
+}
+
+type executionProgressPrinter func([]*TaskStatus)
+
+// Execute executes the given Tasks and the importChangeset statements in the
+// given spec. It regularly calls the executionProgressPrinter with the
+// current TaskStatuses.
+func (c *Coordinator) Execute(ctx context.Context, tasks []*Task, spec *batches.BatchSpec, printer executionProgressPrinter) ([]*batches.ChangesetSpec, []string, error) {
+	var (
+		specs []*batches.ChangesetSpec
+		errs  *multierror.Error
+	)
+
+	// Start the goroutine that updates the UI
+	status := NewTaskStatusCollection(tasks)
+
+	done := make(chan struct{})
+	if printer != nil {
+		go func() {
+			status.CopyStatuses(printer)
+
+			ticker := time.NewTicker(1 * time.Second)
+			defer ticker.Stop()
+
+			for {
+				select {
+				case <-ticker.C:
+					status.CopyStatuses(printer)
+
+				case <-done:
+					return
+				}
+			}
+		}()
+	}
+
+	// Run executor
+	c.exec.Start(ctx, tasks, status)
+	results, err := c.exec.Wait(ctx)
+	if printer != nil {
+		status.CopyStatuses(printer)
+		done <- struct{}{}
+	}
+	if err != nil {
+		if c.opts.SkipErrors {
+			errs = multierror.Append(errs, err)
+		} else {
+			return nil, nil, err
+		}
+	}
+
+	// Write results to cache, build ChangesetSpecs if possible and add to list.
+	for _, taskResult := range results {
+		taskSpecs, err := c.cacheAndBuildSpec(ctx, taskResult, status)
+		if err != nil {
+			return nil, nil, err
+		}
+
+		specs = append(specs, taskSpecs...)
+	}
+
+	// Add external changeset specs.
+	for _, ic := range spec.ImportChangesets {
+		repo, err := c.opts.ResolveRepoName(ctx, ic.Repository)
+		if err != nil {
+			wrapped := errors.Wrapf(err, "resolving repository name %q", ic.Repository)
+			if c.opts.SkipErrors {
+				errs = multierror.Append(errs, wrapped)
+				continue
+			} else {
+				return nil, nil, wrapped
+			}
+		}
+
+		for _, id := range ic.ExternalIDs {
+			var sid string
+
+			switch tid := id.(type) {
+			case string:
+				sid = tid
+			case int, int8, int16, int32, int64:
+				sid = strconv.FormatInt(reflect.ValueOf(id).Int(), 10)
+			case uint, uint8, uint16, uint32, uint64:
+				sid = strconv.FormatUint(reflect.ValueOf(id).Uint(), 10)
+			case float32:
+				sid = strconv.FormatFloat(float64(tid), 'f', -1, 32)
+			case float64:
+				sid = strconv.FormatFloat(tid, 'f', -1, 64)
+			default:
+				return nil, nil, errors.Errorf("cannot convert value of type %T into a valid external ID: expected string or int", id)
+			}
+
+			specs = append(specs, &batches.ChangesetSpec{
+				BaseRepository:    repo.ID,
+				ExternalChangeset: &batches.ExternalChangeset{ExternalID: sid},
+			})
+		}
+	}
+
+	return specs, c.logManager.LogFiles(), errs.ErrorOrNil()
+}

sourcegraph/src-cli

commit 910ac3242345615ef7d1bdbb4a646be3a36e30fe

Distiguishing "finished exec" and "built specs" in TaskStatus
This fixes the regression reported in
https://github.com/sourcegraph/sourcegraph/issues/21230 and introduced
by yours truly in
https://github.com/sourcegraph/src-cli/commit/d6c876cd32397ad53f1e5df1a850fcac7076ba3e.

With the introduction of the Coordinator and the explicit steps of
checking-the-cache-for and building-of ChangesetSpecs outside the
executor, the `TaskStatus.ChangesetSpecs` field wasn't set at the time
when `FinishedAt` was set.

The `batchProgressPrinter` assumed, though, that this was the case and
that if `FinishedAt` was set that `len(taskStatus.ChangesetSpecs) == 0`
means "No changes".

This change here fixes the problem by distinguishing between the two
states: finished execution of steps and finished building changeset
specs.

The problem is that it's still a slight regression in behaviour:
previously the diff stats would be printed in the status bar and in the
verbose mode *as tasks were finishing*.

Now that we build all changeset specs at once, after all of them have
been executed, we can't update the status bar to include diff stats and
the verbose messages will be logged all at once.

I still think that the current code (with the Coordinator) is better
than what we had before and the hard problem here is fixed (no wrong
information being displayed), but longer term I think there's a solution
possible in which we decouple the task execution and its status much
more and make it possible to build better UIs for the status of
execution.

That I think should be approached separately though.

cmd/src/batch_progress_printer.go
@@ -21,8 +21,9 @@ func newBatchProgressPrinter(out *output.Output, verbose bool, numParallelism in
 
 		numParallelism: numParallelism,
 
-		completedTasks: map[string]bool{},
+		executedTasks:  map[string]bool{},
 		runningTasks:   map[string]*executor.TaskStatus{},
+		completedTasks: map[string]bool{},
 
 		repoStatusBar: map[string]int{},
 		statusBarRepo: map[int]string{},
@@ -45,8 +46,14 @@ type batchProgressPrinter struct {
 	maxRepoName    int
 	numParallelism int
 
+	// runningTasks are the tasks that are currently executing.
+	runningTasks map[string]*executor.TaskStatus
+	// executedTasks are the tasks that finished execution but where the
+	// changesetSpec hasn't been built.
+	executedTasks map[string]bool
+	// completedTasks are the tasks that finished execution and finished
+	// building changesetSpecs.
 	completedTasks map[string]bool
-	runningTasks   map[string]*executor.TaskStatus
 
 	repoStatusBar map[string]int
 	statusBarRepo map[int]string
@@ -109,7 +116,8 @@ func (p *batchProgressPrinter) PrintStatuses(statuses []*executor.TaskStatus) {
 		p.numStatusBars = p.initProgressBar(statuses)
 	}
 
-	newlyCompleted := []*executor.TaskStatus{}
+	newlyFinishedExecution := []*executor.TaskStatus{}
+	newlyFinishedBuilding := []*executor.TaskStatus{}
 	currentlyRunning := []*executor.TaskStatus{}
 	errored := 0
 
@@ -118,14 +126,14 @@ func (p *batchProgressPrinter) PrintStatuses(statuses []*executor.TaskStatus) {
 			p.maxRepoName = len(ts.DisplayName())
 		}
 
-		if ts.IsCompleted() {
+		if ts.FinishedExecution() {
 			if ts.Err != nil {
 				errored += 1
 			}
 
-			if !p.completedTasks[ts.DisplayName()] {
-				p.completedTasks[ts.DisplayName()] = true
-				newlyCompleted = append(newlyCompleted, ts)
+			if !p.executedTasks[ts.DisplayName()] {
+				p.executedTasks[ts.DisplayName()] = true
+				newlyFinishedExecution = append(newlyFinishedExecution, ts)
 			}
 
 			if _, ok := p.runningTasks[ts.DisplayName()]; ok {
@@ -137,13 +145,20 @@ func (p *batchProgressPrinter) PrintStatuses(statuses []*executor.TaskStatus) {
 			}
 		}
 
+		if ts.FinishedBuildingSpecs() {
+			if !p.completedTasks[ts.DisplayName()] {
+				p.completedTasks[ts.DisplayName()] = true
+				newlyFinishedBuilding = append(newlyFinishedBuilding, ts)
+			}
+		}
+
 		if ts.IsRunning() {
 			currentlyRunning = append(currentlyRunning, ts)
 		}
 
 	}
 
-	p.updateProgressBar(len(p.completedTasks), errored, len(statuses))
+	p.updateProgressBar(len(p.executedTasks), errored, len(statuses))
 
 	newlyStarted := map[string]*executor.TaskStatus{}
 	statusBarIndex := 0
@@ -176,14 +191,35 @@ func (p *batchProgressPrinter) PrintStatuses(statuses []*executor.TaskStatus) {
 		p.repoStatusBar[ts.DisplayName()] = statusBarIndex
 	}
 
-	for _, ts := range newlyCompleted {
-		fileDiffs, hasDiffs, err := ts.FileDiffs()
-		if err != nil {
-			p.progress.Verbosef("%-*s failed to display status: %s", p.maxRepoName, ts.DisplayName(), err)
-			continue
-		}
+	for _, ts := range newlyFinishedExecution {
+		if idx, ok := p.repoStatusBar[ts.DisplayName()]; ok {
+			// Log that this task completed, but only if there is no
+			// currently executing one in this bar, to avoid flicker.
+			if _, ok := p.statusBarRepo[idx]; !ok {
+				statusText, err := taskStatusBarText(ts)
+				if err != nil {
+					p.progress.Verbosef("%-*s failed to display status: %s", p.maxRepoName, ts.DisplayName(), err)
+					continue
+				}
 
-		if p.verbose {
+				if ts.Err != nil {
+					p.progress.StatusBarFailf(idx, statusText)
+				} else {
+					p.progress.StatusBarCompletef(idx, statusText)
+				}
+			}
+			delete(p.repoStatusBar, ts.DisplayName())
+		}
+	}
+
+	if p.verbose {
+		for _, ts := range newlyFinishedBuilding {
+			fileDiffs, hasDiffs, err := ts.FileDiffs()
+			if err != nil {
+				p.progress.Verbosef("%-*s failed to display status: %s", p.maxRepoName, ts.DisplayName(), err)
+				continue
+			}
+
 			p.progress.WriteLine(output.Linef("", output.StylePending, "%s", ts.DisplayName()))
 
 			if !hasDiffs {
@@ -205,25 +241,6 @@ func (p *batchProgressPrinter) PrintStatuses(statuses []*executor.TaskStatus) {
 			}
 			p.progress.Verbosef("  Execution took %s", ts.ExecutionTime())
 			p.progress.Verbose("")
-		}
-
-		if idx, ok := p.repoStatusBar[ts.DisplayName()]; ok {
-			// Log that this task completed, but only if there is no
-			// currently executing one in this bar, to avoid flicker.
-			if _, ok := p.statusBarRepo[idx]; !ok {
-				statusText, err := taskStatusBarText(ts)
-				if err != nil {
-					p.progress.Verbosef("%-*s failed to display status: %s", p.maxRepoName, ts.DisplayName(), err)
-					continue
-				}
-
-				if ts.Err != nil {
-					p.progress.StatusBarFailf(idx, statusText)
-				} else {
-					p.progress.StatusBarCompletef(idx, statusText)
-				}
-			}
-			delete(p.repoStatusBar, ts.DisplayName())
 		}
 	}
 
@@ -255,24 +272,15 @@ type statusTexter interface {
 func taskStatusBarText(ts *executor.TaskStatus) (string, error) {
 	var statusText string
 
-	if ts.IsCompleted() {
-		diffs, hasDiffs, err := ts.FileDiffs()
-		if err != nil {
-			return "", err
-		}
-
-		if hasDiffs {
-			statusText = diffStatDescription(diffs) + " " + diffStatDiagram(sumDiffStats(diffs))
-		} else {
-			if ts.Err != nil {
-				if texter, ok := ts.Err.(statusTexter); ok {
-					statusText = texter.StatusText()
-				} else {
-					statusText = ts.Err.Error()
-				}
+	if ts.FinishedExecution() {
+		if ts.Err != nil {
+			if texter, ok := ts.Err.(statusTexter); ok {
+				statusText = texter.StatusText()
 			} else {
-				statusText = "No changes"
+				statusText = ts.Err.Error()
 			}
+		} else {
+			statusText = "Done!"
 		}
 	} else if ts.IsRunning() {
 		if ts.CurrentlyExecuting != "" {

internal/batches/executor/coordinator.go
@@ -139,7 +139,15 @@ func (c *Coordinator) checkCacheForTask(ctx context.Context, task *Task) (specs 
 	return specs, true, nil
 }
 
-func (c *Coordinator) cacheAndBuildSpec(ctx context.Context, taskResult taskResult, status taskStatusHandler) ([]*batches.ChangesetSpec, error) {
+func (c *Coordinator) cacheAndBuildSpec(ctx context.Context, taskResult taskResult, status taskStatusHandler) (specs []*batches.ChangesetSpec, err error) {
+	defer func() {
+		// Set these two fields in any case
+		status.Update(taskResult.task, func(status *TaskStatus) {
+			status.ChangesetSpecsDone = true
+			status.ChangesetSpecs = specs
+		})
+	}()
+
 	// Add to the cache, even if no diff was produced.
 	cacheKey := taskResult.task.cacheKey()
 	if err := c.cache.Set(ctx, cacheKey, taskResult.result); err != nil {
@@ -153,17 +161,10 @@ func (c *Coordinator) cacheAndBuildSpec(ctx context.Context, taskResult taskResu
 	}
 
 	// Build the changeset specs.
-	specs, err := createChangesetSpecs(taskResult.task, taskResult.result, c.opts.AutoAuthorDetails)
+	specs, err = createChangesetSpecs(taskResult.task, taskResult.result, c.opts.AutoAuthorDetails)
 	if err != nil {
 		return specs, err
 	}
-
-	// Update the status of Task
-	status.Update(taskResult.task, func(status *TaskStatus) {
-		status.ChangesetSpecs = specs
-		// TODO(mrnugget): Ideally we'd get rid of this dependency on the task
-		// status handler here.
-	})
 
 	return specs, nil
 }
@@ -205,10 +206,6 @@ func (c *Coordinator) Execute(ctx context.Context, tasks []*Task, spec *batches.
 	// Run executor
 	c.exec.Start(ctx, tasks, status)
 	results, err := c.exec.Wait(ctx)
-	if printer != nil {
-		status.CopyStatuses(printer)
-		done <- struct{}{}
-	}
 	if err != nil {
 		if c.opts.SkipErrors {
 			errs = multierror.Append(errs, err)
@@ -225,6 +222,12 @@ func (c *Coordinator) Execute(ctx context.Context, tasks []*Task, spec *batches.
 		}
 
 		specs = append(specs, taskSpecs...)
+	}
+
+	// Now that we've built the specs too we can mark the progress as done
+	if printer != nil {
+		status.CopyStatuses(printer)
+		done <- struct{}{}
 	}
 
 	// Add external changeset specs.

internal/batches/executor/executor.go
@@ -139,7 +139,6 @@ func (x *executor) do(ctx context.Context, task *Task, status taskStatusHandler)
 	// Ensure that the status is updated when we're done.
 	defer func() {
 		status.Update(task, func(status *TaskStatus) {
-			status.FinishedAt = time.Now()
 			status.CurrentlyExecuting = ""
 			status.Err = err
 		})

internal/batches/executor/task_status.go
@@ -67,9 +67,14 @@ type TaskStatus struct {
 	CurrentlyExecuting string
 
 	// ChangesetSpecs are the specs produced by executing the Task in a
-	// repository. With the introduction of `transformChanges` to the batch
-	// spec, one Task can produce multiple ChangesetSpecs.
+	// repository. One Task can produce multiple ChangesetSpecs (see
+	// createChangesetSpec).
+	// Only check this field once ChangesetSpecsDone is set.
 	ChangesetSpecs []*batches.ChangesetSpec
+	// ChangesetSpecsDone is set after the Coordinator attempted to build the
+	// ChangesetSpecs of a task.
+	ChangesetSpecsDone bool
+
 	// Err is set if executing the Task lead to an error.
 	Err error
 
@@ -89,8 +94,12 @@ func (ts *TaskStatus) IsRunning() bool {
 	return !ts.StartedAt.IsZero() && ts.FinishedAt.IsZero()
 }
 
-func (ts *TaskStatus) IsCompleted() bool {
+func (ts *TaskStatus) FinishedExecution() bool {
 	return !ts.StartedAt.IsZero() && !ts.FinishedAt.IsZero()
+}
+
+func (ts *TaskStatus) FinishedBuildingSpecs() bool {
+	return ts.ChangesetSpecsDone
 }
 
 func (ts *TaskStatus) ExecutionTime() time.Duration {
@@ -102,7 +111,7 @@ func (ts *TaskStatus) ExecutionTime() time.Duration {
 // If no file diffs were produced, the task resulted in an error, or the task
 // hasn't finished execution yet, the second return value is false.
 func (ts *TaskStatus) FileDiffs() ([]*diff.FileDiff, bool, error) {
-	if !ts.IsCompleted() || len(ts.ChangesetSpecs) == 0 || ts.Err != nil {
+	if !ts.FinishedBuildingSpecs() || len(ts.ChangesetSpecs) == 0 || ts.Err != nil {
 		return nil, false, nil
 	}
 

cmd/src/batch_progress_printer_test.go
@@ -89,13 +89,44 @@ func TestBatchProgressPrinterIntegration(t *testing.T) {
 		t.Fatalf("wrong output:\n%s", cmp.Diff(want, have))
 	}
 
-	// Now mark the last task as completed
+	// Now mark the last task as finished-execution
 	statuses[len(statuses)-1] = &executor.TaskStatus{
 		RepoName:           "github.com/sourcegraph/automation-testing",
 		StartedAt:          now.Add(time.Duration(-5) * time.Second),
 		FinishedAt:         now.Add(time.Duration(5) * time.Second),
 		CurrentlyExecuting: "",
 		Err:                nil,
+	}
+
+	printer.PrintStatuses(statuses)
+	have = buf.Lines()
+	want = []string{
+		"⠋  Executing... (1/3, 0 errored)  ███████████████▍",
+		"│                                                                               ",
+		"├── github.com/sourcegraph/sourcegraph  echo Hello World > README.md          0s",
+		"├── github.com/sourcegraph/src-cli      Downloading archive                   0s",
+		"└── github.com/sourcegraph/automati...  Done!                                 0s",
+		"",
+	}
+	if !cmp.Equal(want, have) {
+		t.Fatalf("wrong output:\n%s", cmp.Diff(want, have))
+	}
+
+	// Print again to make sure we get the same result
+	printer.PrintStatuses(statuses)
+	have = buf.Lines()
+	if !cmp.Equal(want, have) {
+		t.Fatalf("wrong output:\n%s", cmp.Diff(want, have))
+	}
+
+	// Mark the last task as finished-building-specs
+	statuses[len(statuses)-1] = &executor.TaskStatus{
+		RepoName:           "github.com/sourcegraph/automation-testing",
+		StartedAt:          now.Add(time.Duration(-5) * time.Second),
+		FinishedAt:         now.Add(time.Duration(5) * time.Second),
+		CurrentlyExecuting: "",
+		Err:                nil,
+		ChangesetSpecsDone: true,
 		ChangesetSpecs: []*batches.ChangesetSpec{
 			{
 				BaseRepository: "graphql-id",
@@ -132,16 +163,9 @@ func TestBatchProgressPrinterIntegration(t *testing.T) {
 		"│                                                                               ",
 		"├── github.com/sourcegraph/sourcegraph  echo Hello World > README.md          0s",
 		"├── github.com/sourcegraph/src-cli      Downloading archive                   0s",
-		"└── github.com/sourcegraph/automati...  3 files changed ++++               0s",
+		"└── github.com/sourcegraph/automati...  Done!                                 0s",
 		"",
 	}
-	if !cmp.Equal(want, have) {
-		t.Fatalf("wrong output:\n%s", cmp.Diff(want, have))
-	}
-
-	// Print again to make sure we get the same result
-	printer.PrintStatuses(statuses)
-	have = buf.Lines()
 	if !cmp.Equal(want, have) {
 		t.Fatalf("wrong output:\n%s", cmp.Diff(want, have))
 	}