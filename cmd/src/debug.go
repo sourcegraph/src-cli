77f06d2acfbf43d3aa3f26233d54fc446f7f9767
DaedalusG <warrenbruceg@gmail.com>
2021-08-10 01:19:12 -0700

    populated with TODO tasks, add get pods output

--- cmd/src/debug.go
+++ cmd/src/debug.go
@@ -16,7 +16,7 @@ func init() {
 		fmt.Fprintf(flag.CommandLine.Output(), `'src debug' gathers and bundles debug data from a Sourcegraph deployment.
 
 USAGE
-  src [-v] debug [-out=debug.zip]
+  src [-v] debug [-out=debug.zip] 
 `)
 	}
 
@@ -34,6 +34,11 @@ USAGE
 			return fmt.Errorf("empty -out flag")
 		}
 
+		// TODO: ensures outFile ends in .zip
+		// if *outFile == !strings.HasSuffix(".zip") {
+		//		return fmt.Errorf("-out flg must end in .zip")
+		// }
+
 		out, err := os.OpenFile(*outFile, os.O_CREATE|os.O_RDWR|os.O_EXCL, 0666)
 		if err != nil {
 			return fmt.Errorf("failed to open file: %w", err)
@@ -43,7 +48,8 @@ USAGE
 		zw := zip.NewWriter(out)
 		defer zw.Close()
 
-		k8sEvents, err := zw.Create("src-debug/kubectl/events.txt")
+		// TODO: make outFile the input from `-out=` flag stored in `outFile`
+		k8sEvents, err := zw.Create("outFile/kubectl/events.txt")
 		if err != nil {
 			return fmt.Errorf("failed to create k8s-events.txt: %w", err)
 		}
@@ -55,6 +61,26 @@ USAGE
 		if err := cmd.Run(); err != nil {
 			return fmt.Errorf("running kubectl get events failed: %w", err)
 		}
+
+		// Store logs output in zip
+		k8sPods, err := zw.Create("outFile/kubectl/pods.txt")
+		if err != nil {
+			return fmt.Errorf("failed to create pods.txt: %w", err)
+		}
+
+		// TODO process pods output into array of strings to run in loop as argument for writing as .txt in archive
+		// Get all pod names
+		zmd := exec.Command("kubectl", "get", "pods")
+		zmd.Stdout = k8sPods
+		zmd.Stderr = os.Stderr
+		fmt.Println(k8sPods)
+
+		if err := zmd.Run(); err != nil {
+			return fmt.Errorf("running kubectl get pods failed: %w", err)
+		}
+
+		// Sanity Check
+		fmt.Println("runnnnnnnnn")
 
 		return nil
 	}
