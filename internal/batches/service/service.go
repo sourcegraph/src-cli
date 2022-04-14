bba7dbc8f5fa4753b7772c57601bca3630860342
Adam Harvey <adam@adamharvey.name>
2022-04-14 14:51:30 -0700

    Feature detect Bitbucket Cloud support.

--- internal/batches/service/service.go
+++ internal/batches/service/service.go
@@ -554,7 +554,12 @@ func (svc *Service) ResolveRepositories(ctx context.Context, spec *batcheslib.Ba
 			result.AddRepoRevision(repo.ID, repo)
 
 			switch st := strings.ToLower(repo.ExternalRepository.ServiceType); st {
-			case "github", "gitlab", "bitbucketserver", "bitbucketcloud":
+			case "github", "gitlab", "bitbucketserver":
+			case "bitbucketcloud":
+				if svc.features.BitbucketCloud {
+					break
+				}
+				fallthrough
 			default:
 				if !svc.allowUnsupported {
 					unsupported.Append(repo)
