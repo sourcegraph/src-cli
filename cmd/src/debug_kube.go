4aa5180c343c9c4f0ccabf8b642e5bbfe15a4e7e
Warren Gifford <warrenbruceg@gmail.com>
2022-05-13 11:36:09 -0700

    Update cmd/src/debug_kube.go
    Co-authored-by: Adam Harvey <adam@adamharvey.name>

--- cmd/src/debug_kube.go
+++ cmd/src/debug_kube.go
@@ -241,7 +241,7 @@ func selectPods(ctx context.Context, namespace string) (podList, error) {
 	//Decode json from podList
 	var pods podList
 	if err := json.Unmarshal(podsBuff.Bytes(), &pods); err != nil {
-		errors.Wrap(err, "failed to unmarshall get pods json")
+		return nil, errors.Wrap(err, "failed to unmarshall get pods json")
 	}
 
 	return pods, err
