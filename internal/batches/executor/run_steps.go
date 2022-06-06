692faace0515d8bbf6471a77025f6b794b3cd046
Randell Callahan <randell.callahan@sourcegraph.com>
2022-06-06 12:16:27 -0600

    Fix comment to match actual functionality

--- internal/batches/executor/run_steps.go
+++ internal/batches/executor/run_steps.go
@@ -303,7 +303,7 @@ func executeSingleStep(
 		args = append(args, "--mount", fmt.Sprintf("type=bind,source=%s,target=%s,ro", source.Name(), target))
 	}
 
-	// mount any files on the local system to the docker container
+	// mount any paths on the local system to the docker container
 	for _, mount := range step.Mount {
 		args = append(args, "--mount", fmt.Sprintf("type=bind,source=%s,target=%s,ro", mount.Path, mount.Mountpoint))
 	}
