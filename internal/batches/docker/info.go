1d2ff40e28ab5e9cb212790c6611ab95023af09d
Adam Harvey <adam@adamharvey.name>
2022-06-15 15:10:01 -0700

    Don't bother falling back to GOMAXPROCS.

--- internal/batches/docker/info.go
+++ internal/batches/docker/info.go
@@ -3,7 +3,6 @@ package docker
 import (
 	"bytes"
 	"context"
-	"runtime"
 	"strconv"
 
 	"github.com/sourcegraph/sourcegraph/lib/errors"
@@ -13,25 +12,23 @@ import (
 
 // NCPU returns the number of CPU cores available to Docker.
 func NCPU(ctx context.Context) (int, error) {
-	ncpu := runtime.GOMAXPROCS(0)
-
 	dctx, cancel, err := withFastCommandContext(ctx)
 	if err != nil {
-		return ncpu, err
+		return 0, err
 	}
 	defer cancel()
 
 	args := []string{"info", "--format", "{{ .NCPU }}"}
 	out, err := exec.CommandContext(dctx, "docker", args...).CombinedOutput()
 	if errors.IsDeadlineExceeded(err) || errors.IsDeadlineExceeded(dctx.Err()) {
-		return ncpu, newFastCommandTimeoutError(dctx, args...)
+		return 0, newFastCommandTimeoutError(dctx, args...)
 	} else if err != nil {
-		return ncpu, err
+		return 0, err
 	}
 
 	dcpu, err := strconv.Atoi(string(bytes.TrimSpace(out)))
 	if err != nil {
-		return ncpu, err
+		return 0, err
 	}
 
 	return dcpu, nil
