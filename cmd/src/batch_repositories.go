ded8b16235f4af31863876a8e9afdb73bdb5eaf3
Randell Callahan <piszmogcode@gmail.com>
2022-08-09 14:02:17 -0600

    Remove unused parameter

--- cmd/src/batch_repositories.go
+++ cmd/src/batch_repositories.go
@@ -73,7 +73,7 @@ Examples:
 		}
 
 		out := output.NewOutput(flagSet.Output(), output.OutputOpts{Verbose: *verbose})
-		spec, _, err := parseBatchSpec(ctx, file, svc, false)
+		spec, _, err := parseBatchSpec(ctx, file, svc)
 		if err != nil {
 			ui := &ui.TUI{Out: out}
 			ui.ParsingBatchSpecFailure(err)
