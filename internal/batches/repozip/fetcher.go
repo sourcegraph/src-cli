e65e2289a662006db0f9d865a86ffc35a71abd37
Erik Seliger <erikseliger@me.com>
2022-08-15 23:30:11 +0200

    More safe?

--- internal/batches/repozip/fetcher.go
+++ internal/batches/repozip/fetcher.go
@@ -340,10 +340,16 @@ func fetchRepositoryFile(ctx context.Context, client HTTPClient, repo RepoRevisi
 	if err != nil {
 		return false, err
 	}
-	defer f.Close()
+	// Make sure we clean up the temp file in case something fails.
+	defer func(path string) { _ = os.Remove(path) }(f.Name())
 
 	if _, err := io.Copy(f, resp.Body); err != nil {
+		// Be a good citizen, attempt to close the file.
+		_ = f.Close()
 		return false, err
+	}
+	if err := f.Close(); err != nil {
+		return false, errors.Wrap(err, "closing temp file")
 	}
 
 	// Atomically create the actual file, so that there are no artifacts left behind
