6cc3fe47aa0b8454e41a79934d8f116ea2f051f4
Adam Harvey <adam@adamharvey.name>
2022-08-18 15:07:49 -0700

    For real fix.

--- release.sh
+++ release.sh
@@ -16,7 +16,6 @@ if ! echo "$VERSION" | grep -Eq '^[0-9]+\.[0-9]+\.[0-9]+(-rc\.[0-9]+)?$'; then
   echo "\$VERSION is not in MAJOR.MINOR.PATCH format"
   exit 1
 fi
-exit 0
 
 # Create a new tag and push it, this will trigger the goreleaser workflow in .github/workflows/goreleaser.yml
 git tag "${VERSION}" -a -m "release v${VERSION}"
