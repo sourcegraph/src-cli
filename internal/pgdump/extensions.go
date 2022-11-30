8020472d04b28ceb8f74b25241eb1179437ae6fb
Robert Lin <robert@bobheadxi.dev>
2022-11-30 08:48:59 -0800

    no naked returns

--- internal/pgdump/extensions.go
+++ internal/pgdump/extensions.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"bytes"
 	"io"
+
+	"github.com/sourcegraph/sourcegraph/lib/errors"
 )
 
 // PartialCopyWithoutExtensions will perform a partial copy of a SQL database dump from
@@ -17,23 +19,24 @@ import (
 // Filtering requires reading entire lines into memory - this can be a very expensive
 // operation, so when filtering is complete the more efficient io.Copy should be used
 // to perform the remainder of the copy from src to dst.
-func PartialCopyWithoutExtensions(dst io.Writer, src io.ReadSeeker, progressFn func(int64)) (written int64, err error) {
+func PartialCopyWithoutExtensions(dst io.Writer, src io.ReadSeeker, progressFn func(int64)) (int64, error) {
 	var (
 		reader = bufio.NewReader(src)
 		// position we have consumed up to, track separately because bufio.Reader may have
 		// read ahead on src. This allows us to reset src later.
 		consumed int64
+		// number of bytes we have actually written to dst - it should always be returned.
+		written int64
 		// set to true when we have done all our filtering
 		noMoreExtensions bool
 	)
 
 	for !noMoreExtensions {
 		// Read up to a line, keeping track of our position in src
-		var line []byte
-		line, err = reader.ReadBytes('\n')
+		line, err := reader.ReadBytes('\n')
 		consumed += int64(len(line))
 		if err != nil {
-			return
+			return written, err
 		}
 
 		// Once we start seeing table creations, we are definitely done with extensions,
@@ -47,17 +50,18 @@ func PartialCopyWithoutExtensions(dst io.Writer, src io.ReadSeeker, progressFn f
 		}
 
 		// Write this line and update our progress before returning on error
-		var lineWritten int
-		lineWritten, err = dst.Write(line)
+		lineWritten, err := dst.Write(line)
 		written += int64(lineWritten)
 		progressFn(written)
 		if err != nil {
-			return
+			return written, err
 		}
 	}
 
 	// No more extensions - reset src to the last actual consumed position
-	_, err = src.Seek(consumed, io.SeekStart)
-
-	return
+	_, err := src.Seek(consumed, io.SeekStart)
+	if err != nil {
+		return written, errors.Wrap(err, "reset src position")
+	}
+	return written, nil
 }
