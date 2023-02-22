bd7bcb5e56836fbe23ddf31810488b8f72369601
Jason Hawk Harris <jasonhawkharris@gmail.com>
2023-02-22 16:33:07 -0600

    retab

--- internal/validate/kube/eks.go
+++ internal/validate/kube/eks.go
@@ -1,273 +1,273 @@
 package kube
 
 import (
-	"context"
-	"fmt"
-	"path/filepath"
-	"reflect"
-	"strings"
+    "context"
+    "fmt"
+    "path/filepath"
+    "reflect"
+    "strings"
 
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/tools/clientcmd"
-	"k8s.io/client-go/util/homedir"
+    metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+    "k8s.io/client-go/kubernetes"
+    "k8s.io/client-go/tools/clientcmd"
+    "k8s.io/client-go/util/homedir"
 
-	"github.com/aws/aws-sdk-go-v2/service/ec2"
-	"github.com/aws/aws-sdk-go-v2/service/ec2/types"
-	"github.com/aws/aws-sdk-go-v2/service/eks"
-	"github.com/aws/aws-sdk-go-v2/service/iam"
-	"github.com/sourcegraph/src-cli/internal/validate"
+    "github.com/aws/aws-sdk-go-v2/service/ec2"
+    "github.com/aws/aws-sdk-go-v2/service/ec2/types"
+    "github.com/aws/aws-sdk-go-v2/service/eks"
+    "github.com/aws/aws-sdk-go-v2/service/iam"
+    "github.com/sourcegraph/src-cli/internal/validate"
 
-	"github.com/sourcegraph/sourcegraph/lib/errors"
+    "github.com/sourcegraph/sourcegraph/lib/errors"
 )
 
 type EbsTestObjects struct {
-	addons         []string
-	serviceAccount string
-	ebsRolePolicy  RolePolicy
+    addons         []string
+    serviceAccount string
+    ebsRolePolicy  RolePolicy
 }
 
 // EksEbsCsiDrivers will validate that EKS cluster has ebs-cli drivers installed and configured
 func EksEbsCsiDrivers(ctx context.Context, config *Config) ([]validate.Result, error) {
-	var results []validate.Result
-	var ebsTestParams EbsTestObjects
+    var results []validate.Result
+    var ebsTestParams EbsTestObjects
 
-	addons, err := getAddons(ctx, config.eksClient)
-	if err != nil {
-		results = append(results, validate.Result{
-			Status:  validate.Failure,
-			Message: "EKS: could not validate ebs in addons",
-		})
-		return results, err
-	}
+    addons, err := getAddons(ctx, config.eksClient)
+    if err != nil {
+        results = append(results, validate.Result{
+            Status:  validate.Failure,
+            Message: "EKS: could not validate ebs in addons",
+        })
+        return results, err
+    }
 
-	ebsSA, err := getEbsSA(ctx, config.clientSet)
-	if err != nil {
-		results = append(results, validate.Result{
-			Status:  validate.Failure,
-			Message: "EKS: could not validate ebs service account",
-		})
-		return results, err
-	}
+    ebsSA, err := getEbsSA(ctx, config.clientSet)
+    if err != nil {
+        results = append(results, validate.Result{
+            Status:  validate.Failure,
+            Message: "EKS: could not validate ebs service account",
+        })
+        return results, err
+    }
 
-	EBSCSIRole, err := getEBSCSIRole(ctx, config.iamClient, ebsSA)
-	if err != nil {
-		results = append(results, validate.Result{
-			Status:  validate.Failure,
-			Message: "EKS: could not validate ebs role policy",
-		})
-	}
+    EBSCSIRole, err := getEBSCSIRole(ctx, config.iamClient, ebsSA)
+    if err != nil {
+        results = append(results, validate.Result{
+            Status:  validate.Failure,
+            Message: "EKS: could not validate ebs role policy",
+        })
+    }
 
-	ebsTestParams.addons = addons
-	ebsTestParams.serviceAccount = ebsSA
-	ebsTestParams.ebsRolePolicy = EBSCSIRole
+    ebsTestParams.addons = addons
+    ebsTestParams.serviceAccount = ebsSA
+    ebsTestParams.ebsRolePolicy = EBSCSIRole
 
-	r := validateEbsCsiDrivers(ebsTestParams)
-	results = append(results, r...)
+    r := validateEbsCsiDrivers(ebsTestParams)
+    results = append(results, r...)
 
-	return results, nil
+    return results, nil
 }
 
 func validateEbsCsiDrivers(testers EbsTestObjects) (result []validate.Result) {
-	for _, addon := range testers.addons {
-		if addon == "aws-ebs-csi-driver" {
-			result = append(result, validate.Result{
-				Status:  validate.Success,
-				Message: "EKS: ebs-csi driver validated",
-			})
-		} else {
-			result = append(result, validate.Result{
-				Status:  validate.Failure,
-				Message: "EKS: no 'aws-ebs-csi-driver' present in addons",
-			})
-		}
-	}
+    for _, addon := range testers.addons {
+        if addon == "aws-ebs-csi-driver" {
+            result = append(result, validate.Result{
+                Status:  validate.Success,
+                Message: "EKS: ebs-csi driver validated",
+            })
+        } else {
+            result = append(result, validate.Result{
+                Status:  validate.Failure,
+                Message: "EKS: no 'aws-ebs-csi-driver' present in addons",
+            })
+        }
+    }
 
-	if testers.serviceAccount != "" {
-		result = append(result, validate.Result{
-			Status:  validate.Success,
-			Message: "EKS: ebs service account validated",
-		})
-	} else {
-		result = append(result, validate.Result{
-			Status:  validate.Failure,
-			Message: "EKS: no 'ebs-csi-controller-sa' service account present in cluster",
-		})
-	}
+    if testers.serviceAccount != "" {
+        result = append(result, validate.Result{
+            Status:  validate.Success,
+            Message: "EKS: ebs service account validated",
+        })
+    } else {
+        result = append(result, validate.Result{
+            Status:  validate.Failure,
+            Message: "EKS: no 'ebs-csi-controller-sa' service account present in cluster",
+        })
+    }
 
-	if *testers.ebsRolePolicy.PolicyName == "AmazonEBSCSIDriverPolicy" {
-		result = append(result, validate.Result{
-			Status:  validate.Success,
-			Message: "EKS: service account ebs role policy validated",
-		})
-	} else {
-		result = append(result, validate.Result{
-			Status:  validate.Failure,
-			Message: "EKS: no 'AmazonEBSCSIDriverPolicy' attached to role",
-		})
-	}
+    if *testers.ebsRolePolicy.PolicyName == "AmazonEBSCSIDriverPolicy" {
+        result = append(result, validate.Result{
+            Status:  validate.Success,
+            Message: "EKS: service account ebs role policy validated",
+        })
+    } else {
+        result = append(result, validate.Result{
+            Status:  validate.Failure,
+            Message: "EKS: no 'AmazonEBSCSIDriverPolicy' attached to role",
+        })
+    }
 
-	return result
+    return result
 }
 
 // EksVpc checks if a valid vpc available
 func EksVpc(ctx context.Context, config *Config) ([]validate.Result, error) {
-	var results []validate.Result
-	if config.ec2Client == nil {
-		results = append(results, validate.Result{
-			Status:  validate.Failure,
-			Message: "EKS: validate VPC failed",
-		})
-	}
+    var results []validate.Result
+    if config.ec2Client == nil {
+        results = append(results, validate.Result{
+            Status:  validate.Failure,
+            Message: "EKS: validate VPC failed",
+        })
+    }
 
-	inputs := &ec2.DescribeVpcsInput{}
-	outputs, err := config.ec2Client.DescribeVpcs(ctx, inputs)
+    inputs := &ec2.DescribeVpcsInput{}
+    outputs, err := config.ec2Client.DescribeVpcs(ctx, inputs)
 
-	if err != nil {
-		results = append(results, validate.Result{
-			Status:  validate.Failure,
-			Message: "EKS: Validate VPC failed",
-		})
-		return results, nil
-	}
+    if err != nil {
+        results = append(results, validate.Result{
+            Status:  validate.Failure,
+            Message: "EKS: Validate VPC failed",
+        })
+        return results, nil
+    }
 
-	if len(outputs.Vpcs) == 0 {
-		results = append(results, validate.Result{
-			Status:  validate.Failure,
-			Message: "EKS: Validate VPC failed: No VPC configured",
-		})
-		return results, nil
-	}
+    if len(outputs.Vpcs) == 0 {
+        results = append(results, validate.Result{
+            Status:  validate.Failure,
+            Message: "EKS: Validate VPC failed: No VPC configured",
+        })
+        return results, nil
+    }
 
-	for _, vpc := range outputs.Vpcs {
-		r := validateVpc(&vpc)
-		results = append(results, r...)
-	}
+    for _, vpc := range outputs.Vpcs {
+        r := validateVpc(&vpc)
+        results = append(results, r...)
+    }
 
-	return results, nil
+    return results, nil
 }
 
 func validateVpc(vpc *types.Vpc) (result []validate.Result) {
-	state := vpc.State
+    state := vpc.State
 
-	if state == "available" {
-		result = append(result, validate.Result{
-			Status:  validate.Success,
-			Message: "VPC is validated",
-		})
-		return result
-	}
+    if state == "available" {
+        result = append(result, validate.Result{
+            Status:  validate.Success,
+            Message: "VPC is validated",
+        })
+        return result
+    }
 
-	result = append(result, validate.Result{
-		Status:  validate.Failure,
-		Message: "vpc.State stuck in pending state",
-	})
+    result = append(result, validate.Result{
+        Status:  validate.Failure,
+        Message: "vpc.State stuck in pending state",
+    })
 
-	return result
+    return result
 }
 
 // checks if current context is set to EKS cluster
 func CurrentContextSetToEKSCluster() error {
-	home := homedir.HomeDir()
-	pathToKubeConfig := filepath.Join(home, ".kube", "config")
+    home := homedir.HomeDir()
+    pathToKubeConfig := filepath.Join(home, ".kube", "config")
 
-	config, err := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(
-		&clientcmd.ClientConfigLoadingRules{ExplicitPath: pathToKubeConfig},
-		&clientcmd.ConfigOverrides{
-			CurrentContext: "",
-		}).RawConfig()
+    config, err := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(
+        &clientcmd.ClientConfigLoadingRules{ExplicitPath: pathToKubeConfig},
+        &clientcmd.ConfigOverrides{
+            CurrentContext: "",
+        }).RawConfig()
 
-	if err != nil {
-		return err
-	}
+    if err != nil {
+        return err
+    }
 
-	got := strings.Split(config.CurrentContext, ":")
-	want := []string{"arn", "aws", "eks"}
+    got := strings.Split(config.CurrentContext, ":")
+    want := []string{"arn", "aws", "eks"}
 
-	if len(got) >= 3 {
-		got = got[:3]
-		if reflect.DeepEqual(got, want) {
-			return nil
-		}
-	}
+    if len(got) >= 3 {
+        got = got[:3]
+        if reflect.DeepEqual(got, want) {
+            return nil
+        }
+    }
 
-	return errors.Newf("%s no eks cluster configured", validate.FailureEmoji)
+    return errors.Newf("%s no eks cluster configured", validate.FailureEmoji)
 }
 
 func getAddons(ctx context.Context, client *eks.Client) ([]string, error) {
-	clusterName := getClusterName(ctx, client)
-	inputs := &eks.ListAddonsInput{ClusterName: clusterName}
-	outputs, err := client.ListAddons(ctx, inputs)
+    clusterName := getClusterName(ctx, client)
+    inputs := &eks.ListAddonsInput{ClusterName: clusterName}
+    outputs, err := client.ListAddons(ctx, inputs)
 
-	if err != nil {
-		return nil, err
-	}
+    if err != nil {
+        return nil, err
+    }
 
-	return outputs.Addons, nil
+    return outputs.Addons, nil
 }
 
 func getEbsSA(ctx context.Context, client *kubernetes.Clientset) (string, error) {
-	serviceAccounts := client.CoreV1().ServiceAccounts("kube-system")
-	ebsSA, err := serviceAccounts.Get(
-		ctx,
-		"ebs-csi-controller-sa",
-		metav1.GetOptions{},
-	)
+    serviceAccounts := client.CoreV1().ServiceAccounts("kube-system")
+    ebsSA, err := serviceAccounts.Get(
+        ctx,
+        "ebs-csi-controller-sa",
+        metav1.GetOptions{},
+    )
 
-	if err != nil {
-		return "", err
-	}
+    if err != nil {
+        return "", err
+    }
 
-	annotations := ebsSA.GetAnnotations()
-	roleArn := strings.Split(annotations["eks.amazonaws.com/role-arn"], "/")
-	ebsControllerSA := roleArn[len(roleArn)-1]
+    annotations := ebsSA.GetAnnotations()
+    roleArn := strings.Split(annotations["eks.amazonaws.com/role-arn"], "/")
+    ebsControllerSA := roleArn[len(roleArn)-1]
 
-	return ebsControllerSA, nil
+    return ebsControllerSA, nil
 }
 
 type RolePolicy struct {
-	PolicyName *string
-	PolicyArn  *string
+    PolicyName *string
+    PolicyArn  *string
 }
 
 func getEBSCSIRole(ctx context.Context, client *iam.Client, SAName string) (RolePolicy, error) {
-	inputs := iam.ListAttachedRolePoliciesInput{RoleName: &SAName}
-	outputs, err := client.ListAttachedRolePolicies(ctx, &inputs)
+    inputs := iam.ListAttachedRolePoliciesInput{RoleName: &SAName}
+    outputs, err := client.ListAttachedRolePolicies(ctx, &inputs)
 
-	if err != nil {
-		return RolePolicy{}, err
-	}
+    if err != nil {
+        return RolePolicy{}, err
+    }
 
-	var policyName *string
-	for _, policy := range outputs.AttachedPolicies {
-		policyName = policy.PolicyName
-		if *policyName == "AmazonEBSCSIDriverPolicy" {
-			return RolePolicy{
-				PolicyName: policy.PolicyName,
-				PolicyArn:  policy.PolicyArn,
-			}, nil
-		}
-	}
+    var policyName *string
+    for _, policy := range outputs.AttachedPolicies {
+        policyName = policy.PolicyName
+        if *policyName == "AmazonEBSCSIDriverPolicy" {
+            return RolePolicy{
+                PolicyName: policy.PolicyName,
+                PolicyArn:  policy.PolicyArn,
+            }, nil
+        }
+    }
 
-	return RolePolicy{}, nil
+    return RolePolicy{}, nil
 }
 
 func getClusterName(ctx context.Context, client *eks.Client) *string {
-	home := homedir.HomeDir()
-	pathToKubeConfig := filepath.Join(home, ".kube", "config")
+    home := homedir.HomeDir()
+    pathToKubeConfig := filepath.Join(home, ".kube", "config")
 
-	config, err := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(
-		&clientcmd.ClientConfigLoadingRules{ExplicitPath: pathToKubeConfig},
-		&clientcmd.ConfigOverrides{
-			CurrentContext: "",
-		}).RawConfig()
+    config, err := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(
+        &clientcmd.ClientConfigLoadingRules{ExplicitPath: pathToKubeConfig},
+        &clientcmd.ConfigOverrides{
+            CurrentContext: "",
+        }).RawConfig()
 
-	if err != nil {
-		fmt.Printf("error while checking current context: %s\n", err)
-		return nil
-	}
+    if err != nil {
+        fmt.Printf("error while checking current context: %s\n", err)
+        return nil
+    }
 
-	currentContext := strings.Split(config.CurrentContext, "/")
-	clusterName := currentContext[len(currentContext)-1]
-	return &clusterName
+    currentContext := strings.Split(config.CurrentContext, "/")
+    clusterName := currentContext[len(currentContext)-1]
+    return &clusterName
 }
--- internal/validate/kube/eks_test.go
+++ internal/validate/kube/eks_test.go
@@ -1,155 +1,155 @@
 package kube
 
 import (
-	"github.com/aws/aws-sdk-go-v2/service/ec2/types"
-	"github.com/aws/aws-sdk-go-v2/service/eks"
-	"github.com/sourcegraph/src-cli/internal/validate"
-	"testing"
+    "github.com/aws/aws-sdk-go-v2/service/ec2/types"
+    "github.com/aws/aws-sdk-go-v2/service/eks"
+    "github.com/sourcegraph/src-cli/internal/validate"
+    "testing"
 )
 
 /* func TestValidateEbsCsi(t *testing.T) {
-	cases := []struct {
-		name   string
-		addons func(ListAddonsOutput *eks.ListAddonsOutput)
-		result []validate.Result
-	}{
-		{
-			name: "ebs csi drivers installed",
-			addons: func(ListAddonsOutput *eks.ListAddonsOutput) {
-				ListAddonsOutput.Addons = []string{
-					"aws-ebs-csi-driver",
-				}
-			},
-			result: []validate.Result{
-				{
-					Status:  validate.Success,
-					Message: "EKS: ebs-csi driver validated",
-				},
-			},
-		},
-		{
-			name: "ebs csi drivers not installed",
-			addons: func(ListAddonsOutput *eks.ListAddonsOutput) {
-				ListAddonsOutput.Addons = []string{}
-			},
-			result: []validate.Result{
-				{
-					Status:  validate.Failure,
-					Message: "EKS: validate ebs-csi driver failed",
-				},
-			},
-		},
-	}
+    cases := []struct {
+        name   string
+        addons func(ListAddonsOutput *eks.ListAddonsOutput)
+        result []validate.Result
+    }{
+        {
+            name: "ebs csi drivers installed",
+            addons: func(ListAddonsOutput *eks.ListAddonsOutput) {
+                ListAddonsOutput.Addons = []string{
+                    "aws-ebs-csi-driver",
+                }
+            },
+            result: []validate.Result{
+                {
+                    Status:  validate.Success,
+                    Message: "EKS: ebs-csi driver validated",
+                },
+            },
+        },
+        {
+            name: "ebs csi drivers not installed",
+            addons: func(ListAddonsOutput *eks.ListAddonsOutput) {
+                ListAddonsOutput.Addons = []string{}
+            },
+            result: []validate.Result{
+                {
+                    Status:  validate.Failure,
+                    Message: "EKS: validate ebs-csi driver failed",
+                },
+            },
+        },
+    }
 
-	for _, tc := range cases {
-		tc := tc
-		t.Run(tc.name, func(t *testing.T) {
-			addons := testAddonOutput()
-			if tc.addons != nil {
-				tc.addons(addons)
-			}
-			result := validateEbsCsiDrivers(&addons.Addons)
+    for _, tc := range cases {
+        tc := tc
+        t.Run(tc.name, func(t *testing.T) {
+            addons := testAddonOutput()
+            if tc.addons != nil {
+                tc.addons(addons)
+            }
+            result := validateEbsCsiDrivers(&addons.Addons)
 
-			// test should error
-			if len(tc.result) > 0 {
-				if result == nil {
-					t.Fatal("validate should return result")
-					return
-				}
-				if result[0].Status != tc.result[0].Status {
-					t.Errorf("result status\nwant: %v\n got: %v", tc.result[0].Status, result[0].Status)
-				}
-				if result[0].Message != tc.result[0].Message {
-					t.Errorf("result msg\nwant: %s\n got: %s", tc.result[0].Message, result[0].Message)
-				}
-				return
-			}
+            // test should error
+            if len(tc.result) > 0 {
+                if result == nil {
+                    t.Fatal("validate should return result")
+                    return
+                }
+                if result[0].Status != tc.result[0].Status {
+                    t.Errorf("result status\nwant: %v\n got: %v", tc.result[0].Status, result[0].Status)
+                }
+                if result[0].Message != tc.result[0].Message {
+                    t.Errorf("result msg\nwant: %s\n got: %s", tc.result[0].Message, result[0].Message)
+                }
+                return
+            }
 
-			// test should not error
-			if result != nil {
-				t.Fatalf("ValidateService error: %v", result)
-			}
-		})
-	}
+            // test should not error
+            if result != nil {
+                t.Fatalf("ValidateService error: %v", result)
+            }
+        })
+    }
 } */
 
 func TestValidateVpc(t *testing.T) {
-	cases := []struct {
-		name   string
-		vpc    func(vpc *types.Vpc)
-		result []validate.Result
-	}{
-		{
-			name: "valid vpc",
-			vpc: func(vpc *types.Vpc) {
-				vpc.State = "available"
-			},
-			result: []validate.Result{
-				{
-					Status:  validate.Success,
-					Message: "VPC is validated",
-				},
-			},
-		},
-		{
-			name: "invalid vpc: pending",
-			vpc: func(vpc *types.Vpc) {
-				vpc.State = "pending"
-			},
-			result: []validate.Result{
-				{
-					Status:  validate.Failure,
-					Message: "vpc.State stuck in pending state",
-				},
-			},
-		},
-	}
+    cases := []struct {
+        name   string
+        vpc    func(vpc *types.Vpc)
+        result []validate.Result
+    }{
+        {
+            name: "valid vpc",
+            vpc: func(vpc *types.Vpc) {
+                vpc.State = "available"
+            },
+            result: []validate.Result{
+                {
+                    Status:  validate.Success,
+                    Message: "VPC is validated",
+                },
+            },
+        },
+        {
+            name: "invalid vpc: pending",
+            vpc: func(vpc *types.Vpc) {
+                vpc.State = "pending"
+            },
+            result: []validate.Result{
+                {
+                    Status:  validate.Failure,
+                    Message: "vpc.State stuck in pending state",
+                },
+            },
+        },
+    }
 
-	for _, tc := range cases {
-		tc := tc
-		t.Run(tc.name, func(t *testing.T) {
-			vpc := testVPC()
-			if tc.vpc != nil {
-				tc.vpc(vpc)
-			}
-			result := validateVpc(vpc)
+    for _, tc := range cases {
+        tc := tc
+        t.Run(tc.name, func(t *testing.T) {
+            vpc := testVPC()
+            if tc.vpc != nil {
+                tc.vpc(vpc)
+            }
+            result := validateVpc(vpc)
 
-			// test should error
-			if len(tc.result) > 0 {
-				if result == nil {
-					t.Fatal("validate should return result")
-					return
-				}
-				if result[0].Status != tc.result[0].Status {
-					t.Errorf("result status\nwant: %v\n got: %v", tc.result[0].Status, result[0].Status)
-				}
-				if result[0].Message != tc.result[0].Message {
-					t.Errorf("result msg\nwant: %s\n got: %s", tc.result[0].Message, result[0].Message)
-				}
-				return
-			}
+            // test should error
+            if len(tc.result) > 0 {
+                if result == nil {
+                    t.Fatal("validate should return result")
+                    return
+                }
+                if result[0].Status != tc.result[0].Status {
+                    t.Errorf("result status\nwant: %v\n got: %v", tc.result[0].Status, result[0].Status)
+                }
+                if result[0].Message != tc.result[0].Message {
+                    t.Errorf("result msg\nwant: %s\n got: %s", tc.result[0].Message, result[0].Message)
+                }
+                return
+            }
 
-			// test should not error
-			if result != nil {
-				t.Fatalf("ValidateService error: %v", result)
-			}
-		})
-	}
+            // test should not error
+            if result != nil {
+                t.Fatalf("ValidateService error: %v", result)
+            }
+        })
+    }
 }
 
 // helper test function to return a valid VPC
 func testVPC() *types.Vpc {
-	return &types.Vpc{
-		State: "available",
-	}
+    return &types.Vpc{
+        State: "available",
+    }
 }
 
 // helper test function to return a valid list of add-ons
 // add-ons are checked for ebs csi drivers
 func testAddonOutput() *eks.ListAddonsOutput {
-	return &eks.ListAddonsOutput{
-		Addons: []string{
-			"aws-ebs-csi-driver",
-		},
-	}
+    return &eks.ListAddonsOutput{
+        Addons: []string{
+            "aws-ebs-csi-driver",
+        },
+    }
 }
