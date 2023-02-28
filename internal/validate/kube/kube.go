a51db3ea678c5ba502a96d8be0e076df3ab52530
Jason Hawk Harris <jasonhawkharris@gmail.com>
2023-02-28 11:59:53 -0600

    move imports one level up

--- internal/validate/kube/kube.go
+++ internal/validate/kube/kube.go
@@ -13,8 +13,6 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/kubernetes/scheme"
-	_ "k8s.io/client-go/plugin/pkg/client/auth/azure"
-	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/remotecommand"
 
