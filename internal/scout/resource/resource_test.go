e3dddeff8f8306ae2dc42fe79bac3c19ada28dde
Jason Hawk Harris <jason.harris@sourcegraph.com>
2023-05-16 07:59:00 -0500

    remove unnecessary integration test

--- internal/scout/resource/resource_test.go
+++ internal/scout/resource/resource_test.go
@@ -1,54 +1,9 @@
 package resource
 
 import (
-	"context"
 	"fmt"
 	"testing"
-
-	"github.com/sourcegraph/sourcegraph/lib/errors"
-	v1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/tools/clientcmd"
-	"k8s.io/client-go/util/homedir"
 )
-
-func TestResourcesK8s(t *testing.T) {
-	ctx := context.Background()
-
-	config, err := clientcmd.BuildConfigFromFlags("", homedir.HomeDir()+"/.kube/config")
-	if err != nil {
-		t.Fatal(errors.Wrap(err, "Error getting in cluster config"))
-	}
-
-	k8sClientSet, err := kubernetes.NewForConfig(config)
-	if err != nil {
-		t.Fatal(errors.Wrap(err, "Error creating kubernetes clientset"))
-	}
-
-	// Create some test pods to list
-	pod1 := &v1.Pod{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "pod1",
-			Namespace: "test",
-		},
-	}
-
-	pod2 := &v1.Pod{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "pod2",
-			Namespace: "other",
-		},
-	}
-
-	k8sClientSet.CoreV1().Pods("test").Create(ctx, pod1, metav1.CreateOptions{})
-	k8sClientSet.CoreV1().Pods("other").Create(ctx, pod2, metav1.CreateOptions{})
-
-	err = K8s(ctx, k8sClientSet, nil, WithNamespace("test"))
-	if err != nil {
-		t.Fatal(errors.Wrap(err, "Error calling ResourcesK8s"))
-	}
-}
 
 func TestGetMemUnits(t *testing.T) {
 	cases := []struct {
