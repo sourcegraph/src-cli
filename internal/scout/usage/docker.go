71c4dc2a0c586f5d5a948152bb7e5999fca55372
Jason Hawk Harris <jason.harris@sourcegraph.com>
2023-05-24 14:41:44 -0500

    get list of containers

--- internal/scout/usage/docker.go
+++ internal/scout/usage/docker.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
+	"github.com/sourcegraph/sourcegraph/lib/errors"
 )
 
 func Docker(ctx context.Context, client client.Client, opts ...Option) error {
@@ -14,6 +16,7 @@ func Docker(ctx context.Context, client client.Client, opts ...Option) error {
 		pod:           "",
 		container:     "",
 		spy:           false,
+		restConfig:    nil,
 		k8sClient:     nil,
 		dockerClient:  &client,
 		metricsClient: nil,
@@ -23,6 +26,17 @@ func Docker(ctx context.Context, client client.Client, opts ...Option) error {
 		opt(cfg)
 	}
 
-	fmt.Println("Command is routed, but not yet fixed.")
+	containers, err := cfg.dockerClient.ContainerList(ctx, types.ContainerListOptions{})
+	if err != nil {
+		return errors.Wrap(err, "could not get list of containers")
+	}
+
+	printContainerImages(containers)
 	return nil
+}
+
+func printContainerImages(containers []types.Container) {
+	for _, container := range containers {
+		fmt.Println(container.Image)
+	}
 }
