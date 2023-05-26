1b68b3055b24761f27832d7bee5564672b2c5510
Jason Hawk Harris <jason.harris@sourcegraph.com>
2023-05-26 11:47:30 -0500

    add skeleton for advise command

---
+++ internal/scout/advise/advise.go
@@ -0,0 +1,38 @@
+package advise
+
+import (
+	"github.com/docker/docker/client"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	metricsv "k8s.io/metrics/pkg/client/clientset/versioned"
+)
+
+type Option = func(config *Config)
+type Config struct {
+	namespace     string
+	pod           string
+	container     string
+	docker        bool
+	restConfig    *rest.Config
+	k8sClient     *kubernetes.Clientset
+	dockerClient  *client.Client
+	metricsClient *metricsv.Clientset
+}
+
+func WithNamespace(namespace string) Option {
+	return func(config *Config) {
+		config.namespace = namespace
+	}
+}
+
+func WithPod(podname string) Option {
+	return func(config *Config) {
+		config.pod = podname
+	}
+}
+
+func WithContainer(containerName string) Option {
+	return func(config *Config) {
+		config.container = containerName
+	}
+}
