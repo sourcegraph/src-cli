e6a8502e1b2fa5cf183528912d9b5ec739dd9d56
Jason Hawk Harris <jason.harris@sourcegraph.com>
2023-05-27 14:28:37 -0500

    bulk advise on all pods

--- internal/scout/advise/advise.go
+++ internal/scout/advise/advise.go
@@ -4,6 +4,13 @@ import "github.com/sourcegraph/src-cli/internal/scout"
 
 type Option = func(config *scout.Config)
 
+const (
+	OVER_100 = "\t%s %s: Your %s usage is over 100%% (%.2f%%). Add more %s."
+	OVER_80  = "\t%s %s: Your %s usage is over 80%% (%.2f%%). Consider raising limits."
+	OVER_40  = "\t%s %s: Your %s usage is under 80%% (%.2f%%). Keep %s allocation the same."
+	UNDER_40 = "\t%s %s: Your %s usage is under 40%% (%.2f%%). Consider lowering limits."
+)
+
 func WithNamespace(namespace string) Option {
 	return func(config *scout.Config) {
 		config.Namespace = namespace
--- internal/scout/constants.go
+++ internal/scout/constants.go
@@ -1,5 +1,11 @@
 package scout
 
 const (
-	ABillion float64 = 1000000000
+	ABillion              float64 = 1_000_000_000
+	EmojiFingerPointRight         = "👉"
+	FailureEmoji                  = "🛑"
+	FlashingLightEmoji            = "🚨"
+	HourglassEmoji                = "⌛"
+	SuccessEmoji                  = "✅"
+	WarningSign                   = "⚠️ " // why does this need an extra space to align?!?!
 )
