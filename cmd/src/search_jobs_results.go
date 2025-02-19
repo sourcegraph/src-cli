45339c27e71992742edcd2751ec801d942250bed
Travis Lyons <travis.lyons@sourcegraph.com>
2025-02-19 09:58:22 -0500

    feat: Add search jobs results retrieval command
    Add new command to retrieve search job results in JSONL format with the following capabilities:
    - Fetch results using search job ID
    - Optional file output with -out flag

---
+++ cmd/src/search_jobs_results.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"github.com/sourcegraph/src-cli/internal/api"
+	"github.com/sourcegraph/src-cli/internal/cmderrors"
+)
+
+func init() {
+	usage := `retrieves the results of a search job in JSON Lines (jsonl) format.
+Examples:
+
+	Get the results of a search job:
+	  $ src search-jobs results -id U2VhcmNoSm9iOjY5
+
+	Save search results to a file:
+	  $ src search-jobs results -id U2VhcmNoSm9iOjY5 -out results.jsonl
+`
+
+	flagSet := flag.NewFlagSet("results", flag.ExitOnError)
+	usageFunc := func() {
+		fmt.Fprintf(flag.CommandLine.Output(), "Usage of 'src search-jobs %s':\n", flagSet.Name())
+		flagSet.PrintDefaults()
+		fmt.Println(usage)
+	}
+
+	var (
+		idFlag = flagSet.String("id", "", "ID of the search job to get results for")
+		outFlag = flagSet.String("out", "", "File path to save the results (optional)")
+		apiFlags = api.NewFlags(flagSet)
+	)
+
+	handler := func(args []string) error {
+		if err := flagSet.Parse(args); err != nil {
+			return err
+		}
+
+		if *idFlag == "" {
+			return cmderrors.Usage("must provide a search job ID")
+		}
+
+		client := api.NewClient(api.ClientOpts{
+			Endpoint:    cfg.Endpoint,
+			AccessToken: cfg.AccessToken,
+			Out:         flagSet.Output(),
+			Flags:       apiFlags,
+		})
+
+		job, err := getSearchJob(client, *idFlag)
+		if err != nil {
+			return err
+		}
+
+		if job == nil || job.URL == "" {
+			return fmt.Errorf("no results URL found for search job %s", *idFlag)
+		}
+
+		req, err := http.NewRequest("GET", job.URL, nil)
+		if err != nil {
+			return err
+		}
+
+		req.Header.Add("Authorization", "token "+cfg.AccessToken)
+
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			return err
+		}
+		defer resp.Body.Close()
+
+		if *outFlag != "" {
+			file, err := os.Create(*outFlag)
+			if err != nil {
+				return fmt.Errorf("failed to create output file: %w", err)
+			}
+			defer file.Close()
+
+			_, err = io.Copy(file, resp.Body)
+			if err != nil {
+				return fmt.Errorf("failed to write to output file: %w", err)
+			}
+			return nil
+		}
+
+		_, err = io.Copy(os.Stdout, resp.Body)
+		return err
+	}
+
+	searchJobsCommands = append(searchJobsCommands, &command{
+		flagSet:   flagSet,
+		handler:   handler,
+		usageFunc: usageFunc,
+	})
+}
