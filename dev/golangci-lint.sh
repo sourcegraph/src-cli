7ad28c7074702194a554d2c8b0ccedcec27161bf
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2025-02-21 12:37:32 +0200

    chore/ci: upgrade golanglint-ci to 1.64.5
    Hopefully solves a potential OOM we are seeing on CI.
    
    Test Plan: CI

--- dev/golangci-lint.sh
+++ dev/golangci-lint.sh
@@ -6,7 +6,7 @@ pushd "$(dirname "${BASH_SOURCE[0]}")/.." >/dev/null
 
 mkdir -p dev/.bin
 
-version="1.58.1"
+version="1.64.5"
 suffix="${version}-$(go env GOOS)-$(go env GOARCH)"
 target="$PWD/dev/.bin/golangci-lint-${suffix}"
 url="https://github.com/golangci/golangci-lint/releases/download/v${version}/golangci-lint-${suffix}.tar.gz"
