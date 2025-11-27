2612c77237b8fe86ca28ca09c08eaa74d792f759
William Bezuidenhout <william.bezuidenhout@sourcegraph.com>
2025-11-27 11:23:30 +0200

    add mcp list-tools to list available tool calls

---
+++ cmd/src/mcp.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+)
+
+func init() {
+	flagSet := flag.NewFlagSet("mcp", flag.ExitOnError)
+	handler := func(args []string) error {
+		return mcpMain(args)
+	}
+
+	commands = append(commands, &command{
+		flagSet: flagSet,
+		handler: handler,
+	})
+}
+func mcpMain(args []string) error {
+	fmt.Println("NOTE: This command is still experimental")
+	tools, err := LoadMCPToolDefinitions(mcpToolListJSON)
+	if err != nil {
+		return err
+	}
+
+	subcmd := args[0]
+	if subcmd == "list-tools" {
+		fmt.Println("Available tools")
+		for name := range tools {
+			fmt.Printf("- %s\n", name)
+		}
+		return nil
+	}
+
+	tool, ok := tools[subcmd]
+	if !ok {
+		return fmt.Errorf("tool definition for %q not found - run src mcp list-tools to see a list of available tools", subcmd)
+	}
+	return handleMcpTool(tool, args[1:])
+}
+
+func handleMcpTool(tool *MCPToolDef, args []string) error {
+	fmt.Printf("handling tool %q args: %+v", tool.Name, args)
+	return nil
+}
