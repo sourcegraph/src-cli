146ba0e1e04890687c05231cd75f1fd0a4e3741d
William Bezuidenhout <william.bezuidenhout@sourcegraph.com>
2025-11-28 11:18:33 +0200

    initial handleMcpTool method
    - validate flags with inputSchema

--- cmd/src/mcp.go
+++ cmd/src/mcp.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"strings"
 
 	"github.com/sourcegraph/src-cli/internal/mcp"
 )
@@ -47,6 +48,33 @@ func mcpMain(args []string) error {
 }
 
 func handleMcpTool(tool *mcp.ToolDef, args []string) error {
-	fmt.Printf("handling tool %q args: %+v", tool.Name, args)
+	fs, vars, err := buildArgFlagSet(tool)
+	if err != nil {
+		return err
+	}
+
+	if err := fs.Parse(args); err != nil {
+		return err
+	}
+
+	inputSchema := tool.InputSchema
+
+	for _, reqName := range inputSchema.Required {
+		if vars[reqName] == nil {
+			return fmt.Errorf("no value provided for required flag --%s", reqName)
+		}
+	}
+
+	if len(args) < len(inputSchema.Required) {
+		return fmt.Errorf("not enough arguments provided - the following flags are required:\n%s", strings.Join(inputSchema.Required, "\n"))
+	}
+
+	derefFlagValues(vars)
+
+	fmt.Println("Flags")
+	for name, val := range vars {
+		fmt.Printf("--%s=%v\n", name, val)
+	}
+
 	return nil
 }
