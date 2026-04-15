3bcf8180aa6d08ae555ac4bee052e96d4a2539ce
bahrmichael <michael.bahr@sourcegraph.com>
2026-04-15 12:14:58 +0200

    remove unnecessary tests

--- cmd/src/abc_variables_delete_test.go
+++ cmd/src/abc_variables_delete_test.go
@@ -3,7 +3,7 @@ package main
 import (
 	"testing"
 
-	"github.com/sourcegraph/src-cli/internal/cmderrors"
+	"github.com/google/go-cmp/cmp"
 )
 
 func TestParseABCVariableNames(t *testing.T) {
@@ -20,31 +20,8 @@ func TestParseABCVariableNames(t *testing.T) {
 	if len(variableNames) != 3 {
 		t.Fatalf("len(variableNames) = %d, want 3", len(variableNames))
 	}
-	if variableNames[0] != "approval" || variableNames[1] != "checkpoints" || variableNames[2] != "prompt" {
-		t.Fatalf("variableNames = %#v, want [approval checkpoints prompt]", variableNames)
-	}
-}
-
-func TestParseABCVariableNamesRequiresAtLeastOneName(t *testing.T) {
-	t.Parallel()
-
-	_, err := parseABCVariableNames(nil, nil)
-	if err == nil {
-		t.Fatal("parseABCVariableNames returned nil error, want usage error")
-	}
-	if _, ok := err.(*cmderrors.UsageError); !ok {
-		t.Fatalf("parseABCVariableNames error = %T, want *cmderrors.UsageError", err)
-	}
-}
-
-func TestParseABCVariableNamesRejectsEmptyNames(t *testing.T) {
-	t.Parallel()
-
-	_, err := parseABCVariableNames([]string{"approval", ""}, nil)
-	if err == nil {
-		t.Fatal("parseABCVariableNames returned nil error, want usage error")
-	}
-	if _, ok := err.(*cmderrors.UsageError); !ok {
-		t.Fatalf("parseABCVariableNames error = %T, want *cmderrors.UsageError", err)
+	want := []string{"approval", "checkpoints", "prompt"}
+	if diff := cmp.Diff(want, variableNames); diff != "" {
+		t.Fatalf("variableNames mismatch (-want +got):\n%s", diff)
 	}
 }
